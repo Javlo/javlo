de93734c204db28e24c40be3a3febdec211aba8b
pvandermaesen@noctis.be <pvandermaesen@users.noreply.github.com>
2012-05-09 19:06:20 +0000

    ajax loader animation

--- src/main/webapp/js/edit/ajax.js
+++ src/main/webapp/js/edit/ajax.js
@@ -1,16 +1,18 @@
 jQuery(document).ready(function() {	
 	jQuery("a.ajax").live("click", function(event) {
 		event.preventDefault();
-		var link = jQuery(this);
+		jQuery("#ajax-loader").addClass("active");
+		var link = jQuery(this);		
 		jQuery.ajax({
 			url : link.attr('href').replace("/edit/", "/ajax/"),
 			cache : false,
 			dataType : "json"
-		}).done(function(jsonObj) {
+		}).done(function(jsonObj) {			
 			jQuery.each(jsonObj.zone, function(xhtmlId, xhtml) {
 				jQuery("#" + xhtmlId).html(xhtml);	
 			});			
 			jQuery(document).trigger("ajaxUpdate");
+			jQuery("#ajax-loader").removeClass("active");
 		});
 	});	
 	jQuery(document).trigger("ajaxUpdate");
