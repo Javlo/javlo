c5624e2f6c354d78dd27e7a3710368c568c82279
pvandermaesen@noctis.be <pvandermaesen@users.noreply.github.com>
2012-05-09 16:13:12 +0000

    generic ajax module

--- src/main/webapp/jsp/edit/template/default/js/javlo/core.js
+++ src/main/webapp/jsp/edit/template/default/js/javlo/core.js
@@ -1,21 +1,21 @@
-	jQuery(document).ready(function () {
-		function showTooltip(x, y, contents) {
-			jQuery('<div id="tooltip" class="tooltipflot">' + contents + '</div>').css( {
-				position: 'absolute',
-				display: 'none',
-				top: y + 5,
-				left: x + 5
-			}).appendTo("body").fadeIn(200);
-		}
+jQuery(document).bind("ajaxUpdate",function () {	
+	function showTooltip(x, y, contents) {
+		jQuery('<div id="tooltip" class="tooltipflot">' + contents + '</div>').css( {
+			position: 'absolute',
+			display: 'none',
+			top: y + 5,
+			left: x + 5
+		}).appendTo("body").fadeIn(200);
+	}
 
-		//////////// TABS /////////////////
-		jQuery( ".tabs" ).tabs();
-		
-		//////////// ACCORDION /////////////////
-		jQuery( ".accordion" ).accordion();
-		
-		//////////// DATE PICKER /////////////////
-		jQuery.datepicker.setDefaults( jQuery.datepicker.regional[ editLanguage ] );
-		jQuery( ".datepicker" ).datepicker({maxDate: "+0D", dateFormat: dateFormat}); 
+	//////////// TABS /////////////////
+	jQuery( ".tabs" ).tabs();
+	
+	//////////// ACCORDION /////////////////
+	jQuery( ".accordion" ).accordion( {active:".accordion .open"} );
+	
+	//////////// DATE PICKER /////////////////
+	jQuery.datepicker.setDefaults( jQuery.datepicker.regional[ editLanguage ] );
+	jQuery( ".datepicker" ).datepicker({maxDate: "+0D", dateFormat: dateFormat}); 
 
 });
--- src/main/webapp/modules/content/jsp/component.jsp
+++ src/main/webapp/modules/content/jsp/component.jsp
@@ -1,15 +1,15 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
-<div class="accordion"> <!-- components -->
+<div class="accordion component-list"> <!-- components -->
 <c:set var="closeAccordion" value="" />
 <c:forEach var="comp" items="${components}">
 <c:if test="${comp.metaTitle}">${closeAccordion}
-<h4><a href="#">${i18n.edit[comp.value]}</a></h4>
+<h4${comp.selected?' class="open"':''}><a href="#">${i18n.edit[comp.value]}</a></h4>
 <div><ul>
 <c:set var="closeAccordion" value="</ul></div>"
 /></c:if>
 <c:if test="${not comp.metaTitle}"
-><li><a class="ajax" href="${info.currentURL}?webaction=changeComponent&type=${comp.type}">${comp.label}</a></li>
+><li${comp.selected?' class="selected"':''}><a class="ajax" href="${info.currentURL}?webaction=changeComponent&type=${comp.type}">${comp.label}</a></li>
 </c:if></c:forEach>
 ${closeAccordion}
 </div> <!-- /components -->
