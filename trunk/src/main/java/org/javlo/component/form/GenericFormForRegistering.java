1951d6b6c078919a5da8354947f9cfc01a78c758
pvandermaesen@noctis.be <pvandermaesen@noctis.be@7a838a29-3d20-a625-5319-021463a4e934>
2012-10-02 12:55:16 +0000

    return jstl info if user allready exist

--- trunk/src/main/java/org/javlo/component/form/GenericFormForRegistering.java
+++ trunk/src/main/java/org/javlo/component/form/GenericFormForRegistering.java
@@ -12,46 +12,61 @@ import org.javlo.user.IUserFactory;
 import org.javlo.user.IUserInfo;
 import org.javlo.user.UserFactory;
 import org.javlo.user.UserInfo;
+import org.javlo.user.exception.UserAllreadyExistException;
 
 public class GenericFormForRegistering extends GenericForm {
-	
+
 	@Override
 	protected boolean isCaptcha() {
 		return false;
 	}
-	
-	public static String performSubmit(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		String msg = GenericForm.performSubmit(request, response);
-		
-		ContentContext ctx = ContentContext.getContentContext(request, response);
-		GlobalContext globalContext = GlobalContext.getInstance(ctx.getRequest());
-		IUserFactory userFactory = UserFactory.createUserFactory(globalContext, request.getSession());
-		
-		IUserInfo userInfo = userFactory.createUserInfos();
-		
-		String[] labels = userInfo.getAllLabels();
-		Map<String,String> requestMap = new HashMap<String, String>();
-		for (String label : labels) {
-			String value = request.getParameter(label);
-			requestMap.put(label, value);
-		}		
-		((UserInfo)userInfo).setAllValues(requestMap);
-		
-		if (userInfo.getLogin() == null || userInfo.getLogin().trim().length() == 0) {
-			userInfo.setLogin(userInfo.getEmail());
+
+	public static String performSubmit(HttpServletRequest request, HttpServletResponse response) {
+
+		String msg = null;
+		String email = "?";
+		try {
+			msg = GenericForm.performSubmit(request, response);
+
+			ContentContext ctx = ContentContext.getContentContext(request, response);
+			GlobalContext globalContext = GlobalContext.getInstance(ctx.getRequest());
+			IUserFactory userFactory = UserFactory.createUserFactory(globalContext, request.getSession());
+
+			IUserInfo userInfo = userFactory.createUserInfos();
+
+			String[] labels = userInfo.getAllLabels();
+			Map<String, String> requestMap = new HashMap<String, String>();
+			for (String label : labels) {
+				String value = request.getParameter(label);
+				requestMap.put(label, value);
+			}
+			((UserInfo) userInfo).setAllValues(requestMap);
+			email = userInfo.getEmail();
+
+			if (userInfo.getLogin() == null || userInfo.getLogin().trim().length() == 0) {
+				userInfo.setLogin(userInfo.getEmail());
+			}
+
+			userFactory.addUserInfo(userInfo);
+			userFactory.store();
+		} catch (UserAllreadyExistException e) {
+			request.removeAttribute("valid");
+			request.setAttribute("userExist", email);
+			e.printStackTrace();
+		} catch (Exception e) {
+			request.removeAttribute("valid");
+			request.setAttribute("error", e.getMessage());
+			e.printStackTrace();
 		}
-		
-		userFactory.addUserInfo(userInfo);
-		
+
 		return msg;
 	}
-	
+
 	@Override
-	public String getType() {	
+	public String getType() {
 		return "generic-form-for-registering";
 	}
-	
+
 	@Override
 	public String getActionGroupName() {
 		return "gform-registering";
