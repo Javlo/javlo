b1ddc53aea0aa617cf237fc949a206e2d535cd1c
pvandermaesen@noctis.be <pvandermaesen@noctis.be@7a838a29-3d20-a625-5319-021463a4e934>
2012-10-24 21:01:27 +0000

    import data 2.0.0.9

--- trunk/src/main/java/org/javlo/servlet/AccessServlet.java
+++ trunk/src/main/java/org/javlo/servlet/AccessServlet.java
@@ -84,7 +84,7 @@ public class AccessServlet extends HttpServlet {
 	 */
 	public static Logger logger = Logger.getLogger(AccessServlet.class.getName());
 
-	public static final String VERSION = "2.0.0.8";
+	public static final String VERSION = "2.0.0.9";
 
 	@Override
 	public void destroy() {
