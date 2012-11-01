b5899967401f482d5069f07be2d5979eb02ac8ac
pvandermaesen@noctis.be <pvandermaesen@users.noreply.github.com>
2012-11-01 21:58:01 +0000

    field import

--- src/main/java/org/javlo/fields/FieldDate.java
+++ src/main/java/org/javlo/fields/FieldDate.java
@@ -3,16 +3,21 @@ package org.javlo.fields;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import org.javlo.component.core.IDate;
 import org.javlo.context.ContentContext;
 import org.javlo.helper.StringHelper;
 import org.javlo.helper.XHTMLHelper;
 
+public class FieldDate extends Field implements IDate {
 
-public class FieldDate extends Field {
-	
 	@Override
 	public boolean validate() {
+		if (getValue() == null || getValue().trim().length() == 0) {
+			return true;
+		}
 		try {
 			StringHelper.parseDate(getValue());
 		} catch (ParseException e) {
@@ -24,25 +29,47 @@ public class FieldDate extends Field {
 	}
 
 	@Override
+	public Date getDate(ContentContext ctx) {
+		try {
+			if (getValue() != null && getValue().trim().length() > 0) {
+				return StringHelper.parseDate(getValue());
+			}
+		} catch (ParseException e) {
+		}
+		return null;
+	}
+
+	@Override
 	public String getViewXHTMLCode(ContentContext ctx) {
 		StringWriter writer = new StringWriter();
 		PrintWriter out = new PrintWriter(writer);
-		
+
 		String displayStr = StringHelper.neverNull(getValue());
 		if (displayStr.trim().length() == 0 || !isViewDisplayed()) {
 			return "";
 		}
-		out.println("<p class=\"" + getType() + " " + getName() + "\">");
-		out.println(XHTMLHelper.textToXHTML(displayStr));
-		out.println("</p>");
+
+		String format = getMetaData("format");
+
+		if (format == null) {
+			out.println(XHTMLHelper.textToXHTML(displayStr));
+		} else {
+			SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+			try {
+				out.println(dateFormat.format(StringHelper.parseDate(displayStr)));
+			} catch (ParseException e) {
+				e.printStackTrace();
+				return e.getMessage();
+			}
+		}
 
 		out.close();
 		return writer.toString();
 	}
-	
+
+	@Override
 	public String getType() {
 		return "date";
 	}
 
 }
-
