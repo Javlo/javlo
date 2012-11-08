3ccc35bf7dc6c0b7265e1639872071a8aa69e7c3
pvandermaesen@noctis.be <pvandermaesen@users.noreply.github.com>
2012-11-08 22:50:18 +0000

    work list of MenuElement as List in place of Array jalvo work without portlet lib preview edition work with template with absolute reference to jQuery lib

--- src/main/java/org/javlo/servlet/AccessServlet.java
+++ src/main/java/org/javlo/servlet/AccessServlet.java
@@ -84,7 +84,7 @@ public class AccessServlet extends HttpServlet {
 	 */
 	public static Logger logger = Logger.getLogger(AccessServlet.class.getName());
 
-	public static final String VERSION = "2.0.0.9";
+	public static final String VERSION = "2.0.0.10";
 
 	@Override
 	public void destroy() {
