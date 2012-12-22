f5899c9fe24746eaa9e187bd2638d8b0ea770ab4
pvandermaesen@noctis.be <pvandermaesen@noctis.be@7a838a29-3d20-a625-5319-021463a4e934>
2012-12-22 10:50:44 +0000

    transform image in //

--- trunk/src/main/java/org/javlo/servlet/AccessServlet.java
+++ trunk/src/main/java/org/javlo/servlet/AccessServlet.java
@@ -84,7 +84,7 @@ public class AccessServlet extends HttpServlet {
 	 */
 	public static Logger logger = Logger.getLogger(AccessServlet.class.getName());
 
-	public static final String VERSION = "2.0.0.12";
+	public static final String VERSION = "2.0.0.13";
 
 	@Override
 	public void destroy() {
