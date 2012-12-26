0dd706109571f28551676e376e25a367763c0780
pvandermaesen@noctis.be <pvandermaesen@users.noreply.github.com>
2012-12-26 18:04:51 +0000

    upload image with drag/drop in GlobalImage

--- src/main/java/org/javlo/context/GlobalContext.java
+++ src/main/java/org/javlo/context/GlobalContext.java
@@ -97,7 +97,6 @@ public class GlobalContext implements Serializable {
 
 		@Override
 		public void run() {
-
 			while (!globalContext.stopStoreThread) {
 				if (globalContext.needStoreData) {
 					globalContext.needStoreData = false;
