ea7860d46289007c6c1fc90c9a37c64dab167dfe
pvandermaesen@noctis.be <pvandermaesen@users.noreply.github.com>
2013-01-16 23:31:09 +0000

    don't display unknow component in view mode.

--- src/main/java/org/javlo/component/core/Unknown.java
+++ src/main/java/org/javlo/component/core/Unknown.java
@@ -9,19 +9,19 @@ import org.javlo.context.ContentContext;
  * @author pvandermaesen
  */
 public class Unknown extends AbstractVisualComponent {
-	
+
 	String type = "unknow";
-	
-	public Unknown ( ContentContext newCtx, ComponentBean bean  ) throws Exception {
-		init(bean, newCtx );		
-	}	
+
+	public Unknown(ContentContext newCtx, ComponentBean bean) throws Exception {
+		init(bean, newCtx);
+	}
 
 	@Override
 	protected String getEditXHTMLCode(ContentContext ctx) throws Exception {
 		StringBuffer finalCode = new StringBuffer();
 		finalCode.append(getSpecialInputTag());
 		finalCode.append("<br />");
-		finalCode.append("<b>&nbsp;not  found : "+getComponentBean().getType()+"</b>");
+		finalCode.append("<b>&nbsp;not  found : " + getComponentBean().getType() + "</b>");
 		finalCode.append("<br /><br />");
 		finalCode.append("<textarea id=\"" + getContentName() + "\" name=\"" + getContentName() + "\"");
 		finalCode.append(" rows=\"" + (countLine() + 1) + "\" onkeyup=\"javascript:resizeTextArea($('" + getContentName() + "'));\">");
@@ -30,7 +30,16 @@ public class Unknown extends AbstractVisualComponent {
 
 		return finalCode.toString();
 	}
-	
+
+	@Override
+	public String getViewXHTMLCode(ContentContext ctx) throws Exception {
+		if (ctx.isAsViewMode()) {
+			return "";
+		} else {
+			return super.getViewXHTMLCode(ctx);
+		}
+	}
+
 	@Override
 	public String getXHTMLConfig(ContentContext ctx) throws Exception {
 		return "?";
@@ -39,7 +48,8 @@ public class Unknown extends AbstractVisualComponent {
 	/*
 	 * @see org.javlo.itf.IContentVisualComponent#getType()
 	 */
-	public String getType() {		
+	@Override
+	public String getType() {
 		return type;
 	}
 }
