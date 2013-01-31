a3150c3396a2d4fd2d3663f5adee8a66d9898493
pvandermaesen@noctis.be <pvandermaesen@users.noreply.github.com>
2013-01-31 21:03:17 +0000

    import jcr xml

--- src/main/java/org/javlo/macro/ImportZIPPageMacro.java
+++ src/main/java/org/javlo/macro/ImportZIPPageMacro.java
@@ -1,25 +1,127 @@
 package org.javlo.macro;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
+import java.util.Enumeration;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
 import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
 
+import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.htmlparser.Node;
 import org.htmlparser.util.NodeList;
 import org.javlo.actions.IAction;
+import org.javlo.component.core.ComponentBean;
+import org.javlo.component.meta.DateComponent;
+import org.javlo.component.text.WysiwygParagraph;
+import org.javlo.component.title.Title;
+import org.javlo.config.StaticConfig;
 import org.javlo.context.ContentContext;
 import org.javlo.context.GlobalContext;
+import org.javlo.helper.MacroHelper;
+import org.javlo.helper.StringHelper;
+import org.javlo.helper.URLHelper;
 import org.javlo.i18n.I18nAccess;
 import org.javlo.macro.core.IInteractiveMacro;
 import org.javlo.message.MessageRepository;
+import org.javlo.navigation.MenuElement;
+import org.javlo.service.ContentService;
 import org.javlo.service.RequestService;
+import org.javlo.xml.NodeXML;
+import org.javlo.xml.XMLFactory;
 
 public class ImportZIPPageMacro implements IInteractiveMacro, IAction {
 
+	public static class Config {
+		private Config() {
+		};
+
+		static Properties getConfig(ContentContext ctx) throws Exception {
+			if (ctx.getCurrentTemplate() != null) {
+				return ctx.getCurrentTemplate().getMacroProperties(GlobalContext.getInstance(ctx.getRequest()), "import-zip");
+			} else {
+				return null;
+			}
+		}
+
+		private static File getImportFolder(ContentContext ctx) throws Exception {
+			Properties config = getConfig(ctx);
+			StaticConfig staticConfig = StaticConfig.getInstance(ctx.getRequest().getSession());
+			File dir = new File(staticConfig.replaceFolderVariable("$HOME/data/import"));
+			if (config != null) {
+				dir = new File(config.getProperty("folder", staticConfig.replaceFolderVariable("$HOME/data/import")));
+			}
+			return dir;
+		}
+
+		private static String getProperty(ContentContext ctx, String key, String defaultValue) {
+			Properties prop;
+			try {
+				prop = getConfig(ctx);
+			} catch (Exception e) {
+				e.printStackTrace();
+				return defaultValue;
+			}
+			if (prop == null) {
+				return defaultValue;
+			} else {
+				return prop.getProperty(key, defaultValue);
+			}
+		}
+
+		private static String getTitleXPath(ContentContext ctx) {
+			return getProperty(ctx, "title.xpath", ".//subTitle/@value");
+		}
+
+		private static String getDateXPath(ContentContext ctx) {
+			return getProperty(ctx, "date.xpath", ".//articleDate/@value");
+		}
+
+		private static String getContentXPath(ContentContext ctx) {
+			return getProperty(ctx, "content.xpath", ".//textContent/@value");
+		}
+
+		private static String getDateFormat(ContentContext ctx) {
+			return getProperty(ctx, "date.format", "yyy-MM-dd'T'HH:mm:ss");
+		}
+
+		private static String getPageRoot(ContentContext ctx) {
+			return getProperty(ctx, "page.root", "articles");
+		}
+
+	}
+
 	private static Logger logger = Logger.getLogger(ImportZIPPageMacro.class.getName());
+
+	public static final class Page {
+		private final String name;
+		private final String label;
+
+		public Page(String name, String label) {
+			this.name = name;
+			this.label = label;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String getLabel() {
+			return label;
+		}
+
+	}
 
 	@Override
 	public String getName() {
@@ -63,28 +165,103 @@ public class ImportZIPPageMacro implements IInteractiveMacro, IAction {
 	}
 
 	public static String performImport(RequestService rs, ContentContext ctx, GlobalContext globalContext, MessageRepository messageRepository, I18nAccess i18nAccess) throws MalformedURLException, Exception {
+
+		File fileToImport = new File(URLHelper.mergePath(Config.getImportFolder(ctx).getAbsolutePath(), rs.getParameter("file", "_____")));
+		if (!fileToImport.exists()) {
+			return "file not found : " + fileToImport;
+		} else {
+			importFile(ctx, fileToImport);
+		}
+
 		return null;
 	}
 
-	static Properties getConfig(ContentContext ctx) throws Exception {
-		if (ctx.getCurrentTemplate() != null) {
-			return ctx.getCurrentTemplate().getMacroProperties(GlobalContext.getInstance(ctx.getRequest()), "import-zip");
-		} else {
-			return null;
+	private static Locale getLocalBySuffix(String name) {
+		if (name.contains("_")) {
+			String[] splitted = name.split("_");
+			if (splitted.length == 2) {
+				if (splitted[1].length() == 2) {
+					return new Locale(splitted[1]);
+				}
+			} else if (splitted.length > 2) {
+				if (splitted[splitted.length - 2].length() == 2) {
+					return new Locale(splitted[splitted.length - 2], splitted[splitted.length - 1]);
+				} else {
+					return new Locale(splitted[splitted.length - 1]);
+				}
+			}
 		}
+		return null;
+	}
+
+	private static String importFile(ContentContext ctx, File zip) throws ZipException, IOException {
+		ZipFile zipFile = new ZipFile(zip);
+		Enumeration<? extends ZipEntry> entries = zipFile.entries();
+		String pageName = StringHelper.getFileNameWithoutExtension(zip.getName());
+		while (entries.hasMoreElements()) {
+			ZipEntry entry = entries.nextElement();
+			if (!entry.isDirectory() && entry.getName().endsWith(".xml")) {
+				String fileName = entry.getName().replace(".xml", "");
+				Locale locale = getLocalBySuffix(fileName);
+				if (locale != null) {
+					try {
+						NodeXML node = XMLFactory.getFirstNode(zipFile.getInputStream(entry));
+						String title = node.searchValue(Config.getTitleXPath(ctx));
+						String dateStr = node.searchValue(Config.getDateXPath(ctx));
+						SimpleDateFormat format = new SimpleDateFormat(Config.getDateFormat(ctx));
+						Date date = format.parse(dateStr);
+						String xhtml = node.searchValue(Config.getContentXPath(ctx));
+
+						ContentService content = ContentService.getInstance(ctx.getRequest());
+						MenuElement page = content.getNavigation(ctx).searchChildFromName(pageName);
+						MenuElement rootPage = content.getNavigation(ctx).searchChildFromName(Config.getPageRoot(ctx));
+						if (rootPage != null) {
+							if (page == null) {
+								page = MacroHelper.createArticlePage(ctx, rootPage, date);
+								page.setName(pageName);
+								ctx.setPath(page.getPath());
+							}
+							logger.info("create page : in " + locale + " " + page.getPath());
+							String compId = content.createContent(ctx, page, new ComponentBean(Title.TYPE, title, locale.getLanguage()), "0", false);
+							compId = content.createContent(ctx, page, new ComponentBean(DateComponent.TYPE, StringHelper.renderTime(date), locale.getLanguage()), compId, false);
+							compId = content.createContent(ctx, page, new ComponentBean(WysiwygParagraph.TYPE, xhtml, locale.getLanguage()), compId, false);
+						} else {
+							return "page not found : " + Config.getPageRoot(ctx);
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		return null;
 	}
 
 	@Override
 	public String prepare(ContentContext ctx) {
 		try {
-			Properties config = getConfig(ctx);
-			if (config != null) {
-
+			File dir = Config.getImportFolder(ctx);
+			List<Page> pages = new LinkedList<Page>();
+			if (dir.exists() && dir.isDirectory()) {
+				for (File file : dir.listFiles((FileFilter) FileFilterUtils.suffixFileFilter(".zip"))) {
+					pages.add(new Page(file.getName(), file.getName().replace(".zip", "")));
+				}
+				ctx.getRequest().setAttribute("pages", pages);
+				return null;
+			} else {
+				return "" + dir + " is not a folder.";
 			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			return e.getMessage();
 		}
-		return null;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("***** ImportZIPPageMacro.main : locale = " + getLocalBySuffix("index_fr")); // TODO: remove debug trace
+		System.out.println("***** ImportZIPPageMacro.main : locale = " + getLocalBySuffix("index_fr_BE")); // TODO: remove debug trace
+		System.out.println("***** ImportZIPPageMacro.main : locale = " + getLocalBySuffix("index_frdfsf")); // TODO: remove debug trace
+		System.out.println("***** ImportZIPPageMacro.main : locale = " + getLocalBySuffix("index")); // TODO: remove debug trace
 	}
 }
--- src/main/java/org/javlo/xml/NodeXML.java
+++ src/main/java/org/javlo/xml/NodeXML.java
@@ -1,5 +1,10 @@
 package org.javlo.xml;
 
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Attr;
 import org.w3c.dom.CDATASection;
@@ -10,301 +15,328 @@ import org.w3c.dom.Text;
 
 //TODO Adding javadoc could be helpfull.
 /**
- *
- * @author  pvanderm
+ * 
+ * @author pvanderm
  * @version 1.0
  */
 public class NodeXML extends Object implements Cloneable {
-    
-    Node node;
-    Document document;
-    
-    public static boolean DEBUG = false;
-    
-    /** Creates new NodeXML */
-    public NodeXML(Node node)  {
-        this.node = node;
-        this.document = node.getOwnerDocument();
-    }
-    
-    public NodeXML(Document doc) {
-        this.document = doc;
-        this.node = document.getDocumentElement();
-    }
-    
-    /**
-     * get the first child node (pvdm)
-     */
-    public NodeXML getChild() {
-        Node node=this.node;
-        node=node.getFirstChild();
-        if (node != null) {
-            return new NodeXML(node).getNext();
-        } else {
-            return null;
-        }
-    }
-    
-    /**
-     * get the first child node with a specific name (gh)
-     * @param name name of the node needed (gh)
-     * @return the node as a NodeXML if matching name, null otherwise (gh)
-     */
-    public NodeXML getChild(String name) {
-        Node node=this.node;
-        node=node.getFirstChild();
-        while (node != null) {
-            while ((node != null) && !(node instanceof Element)) {
-                node=node.getNextSibling();
-            }
-            if(node != null) {
-                if (node.getNodeName().equals(name)) {
-                    return new NodeXML(node);
-                } else {
-                    node=node.getNextSibling();
-                }
-            }
-        }
-        return null;
-    }
-    
-    /**
-     * get the first child node (pvdm)
-     */
-    public NodeXML getParent() {
-        Node node=this.node;
-        node=node.getParentNode();
-        if (node != null) {
-            return new NodeXML(node);
-        } else {
-            return null;
-        }
-    }
-    
-    
-    /**
-     * get the next node (pvdm)
-     */
-    public NodeXML getNext() {
-        Node node=this.node;
-        node=node.getNextSibling();
-        while (! (node instanceof Element) && (node != null)) {
-            node=node.getNextSibling();
-        }
-        if (node != null) {
-            return new NodeXML(node);
-        } else {
-            return null;
-        }
-    }
-    
-    /**
-     * get the next node (pvdm)
-     * @param name return the next node with this name (pvdm)
-     */
-    public NodeXML getNext(String name) {
-        NodeXML res=null;
-        Node node=this.node;
-        node=node.getNextSibling();
-        while (/*! (node instanceof Element) &&*/ (node != null) && (!node.getNodeName().equals(name))) {
-            node=node.getNextSibling();
-        }
-        if (node != null) {
-            if (node.getNodeName().equals(name)) {
-                res = new NodeXML(node);
-            }
-        }
-        return res;
-    }
-    
-    /**
-     * @return the name of the node (pvdm)
-     */
-    public String getName() {
-        return node.getNodeName();
-    }
-    
-    /**
-     * get a attribute value (pvdm)
-     */
-    public String getAttributeValue(String attribute) {
-        if (((Element)node).getAttributeNode(attribute) == null) {
-            error("Attribute '"+attribute+"' not found on '"+node.getNodeName()+"' node.");
-        } else {
-            if (node instanceof Element) {
-                return ((Element)node).getAttributeNode(attribute).getValue();
-            }
-        }
-        return null;
-    }
-    
-    /**
-     * get a attribute value, with a standard value if the attribute is not found.
-     * @param attribute the name of the attribute
-     * @param standardValue the standard value if attribute not found.
-     * @return the attribute value if exist else the standard value.
-     */
-    public String getAttributeValue( String attribute, String standardValue ) {
-        String res = getAttributeValue( attribute );
-        if ( res == null ) {
-            res=standardValue;
-        }
-        return res;
-    }
-    
-    /**
-     * get a attribute value (pvdm)
-     */
-    public boolean attributeExist(String attribute) {
-        return ((Element)node).getAttributeNode(attribute) != null;
-    }
-    
-    /**
-     * print error message on the screen (pvdm)
-     */
-    protected void error(String message) {
-        if (DEBUG) {
-            System.out.println("ERROR : "+message);
-        }
-    }
-    
-    
-    /**
-     * search a node with a specific value for a parameter (pvdm)
-     * @param name the name of the node (pvdm)
-     * @param paramName the name of the attribute of the node (pvdm)
-     * @param paramValue the value of the attribute (pvdm)
-     * @return renturn the node fund with this specification (pvdm)
-     */
-    public NodeXML searchChild(String name, String paramName, String paramValue) {
-        NodeXML res=null;
-        NodeXML child;
-        child = getChild(name);
-        while (child != null) {
-            if (child.getAttributeValue(paramName).equals(paramValue)) {
-                res=child;
-            }
-            child = child.getNext(name);
-        }
-        return res;
-    }
-    
-    /**
-     * get the first text content of the node, if any (grh)
-     */
-    public String getContent() {
-        String res="";  
-        Node node=this.node;
-        node=node.getFirstChild();
-        while (node != null) {
-            if (node instanceof Text || node instanceof CDATASection) {
-                res = res + node.getNodeValue();
-            }
-            node=node.getNextSibling();
 
-        }
-        if (res.length() > 0) {
-            return res;
-        } else {
-            return null;
-        }
-    }
-    
-    public Document getOwnerDocument() {
-        return document;
-    }
-    
-    public Node getNode() {
-        return node;
-    }
-    
-    public NodeXML addChild( String name ) {
-        Node newNode = document.createElement(name);
-        newNode = node.appendChild( newNode );
-        return new NodeXML( newNode );
-    }
+	Node node;
+	Document document;
 
-    /**
-     * Method addAttribute adds an attribute to a node with all attributes
-     * value to null, so you still have to do a setAttribute to add value.
-     * @param name name of the new attribute.
-     */
-    public void addAttribute(String name ) {
-        Attr newAttr = document.createAttribute(name);
-        /*Node newNode = */((Element) node).setAttributeNode(newAttr);
-//        return new NodeXML(newNode);
-    }
-    
-    /**
-     * Method setAttribute sets an attrinute to its value, if the attribute
-     * does not exist it will be added. @see 
-     * @param name of the attribute.
-     * @param value of the attribute.
-     */
-    public void setAttribute( String name, String value ) {
-        Element element = (Element)node;
-        element.setAttribute( name, value );
-    }
-    
-    public void setContent( String content ) {
-        Node newNode = node.getFirstChild();
-        if ( ( newNode != null ) && ( newNode instanceof Text ) ) {
-            newNode.setNodeValue(content);
-        } else {
-            newNode = document.createTextNode(content);
-            node.appendChild(newNode);
-        }
-    }
+	public static boolean DEBUG = false;
 
-/**
- * setCDATAContent: Set a CDATA content in a DOM, Will add a new CDATA node if there is none.
- * (xcosyns)
- * @param content The content to have as a CDATA section in your node. 
- */
-    public void setCDATAContent(String content ) {
-        Node newNode = node.getFirstChild();
-        if ( ( newNode != null ) && ( newNode instanceof CDATASection ) ) {
-            newNode.setNodeValue(content);
-        } else {
-            newNode = document.createCDATASection(content);
-            node.appendChild(newNode);
-        }
-    }
+	/** Creates new NodeXML */
+	public NodeXML(Node node) {
+		this.node = node;
+		this.document = node.getOwnerDocument();
+	}
 
-    
-    public void removeChild( NodeXML n ) {
-        node.removeChild( n.getNode() );
-    }
+	public NodeXML(Document doc) {
+		this.document = doc;
+		this.node = document.getDocumentElement();
+	}
 
-    /**
-     * @see java.lang.Object#clone()
-     * Clones this XMLNode with the 'deep' attribute set. So it clones this node
-     * and the whole subtree of nodes.
-     * @author xcosyns
-     */
-    public Object clone(){
-        try{
-            NodeXML clonedNodeXML = (NodeXML) super.clone();
-//            clonedNodeXML.node = this.node.cloneNode(true);
-            clonedNodeXML.document = (Document) this.document.cloneNode(true);
-//            clonedNodeXML.node = clonedNodeXML.document.importNode(this.node, true);
-            clonedNodeXML.node = clonedNodeXML.document.getDocumentElement();
-            return clonedNodeXML;    
-        } catch (CloneNotSupportedException e){
-            // this shouldn't happen because NodeXML is Cloneable
-            throw new InternalError();
-        }
-    }
-    
-    public String nodeToString(){
-        StringBuffer sb = new StringBuffer("NodeXML Name = ");
-        sb.append(node.getNodeName());
-        sb.append(" NodeXML value = ");
-        sb.append(node.getNodeValue());
-        sb.append(" Content ");
-        sb.append(this.getContent());
-        sb.append("\n");
-        return sb.toString();          
-    }
-    
+	/**
+	 * get the first child node (pvdm)
+	 */
+	public NodeXML getChild() {
+		Node node = this.node;
+		node = node.getFirstChild();
+		if (node != null) {
+			return new NodeXML(node).getNext();
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * get the first child node with a specific name (gh)
+	 * 
+	 * @param name
+	 *            name of the node needed (gh)
+	 * @return the node as a NodeXML if matching name, null otherwise (gh)
+	 */
+	public NodeXML getChild(String name) {
+		Node node = this.node;
+		node = node.getFirstChild();
+		while (node != null) {
+			while ((node != null) && !(node instanceof Element)) {
+				node = node.getNextSibling();
+			}
+			if (node != null) {
+				if (node.getNodeName().equals(name)) {
+					return new NodeXML(node);
+				} else {
+					node = node.getNextSibling();
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * get the first child node (pvdm)
+	 */
+	public NodeXML getParent() {
+		Node node = this.node;
+		node = node.getParentNode();
+		if (node != null) {
+			return new NodeXML(node);
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * get the next node (pvdm)
+	 */
+	public NodeXML getNext() {
+		Node node = this.node;
+		node = node.getNextSibling();
+		while (!(node instanceof Element) && (node != null)) {
+			node = node.getNextSibling();
+		}
+		if (node != null) {
+			return new NodeXML(node);
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * get the next node (pvdm)
+	 * 
+	 * @param name
+	 *            return the next node with this name (pvdm)
+	 */
+	public NodeXML getNext(String name) {
+		NodeXML res = null;
+		Node node = this.node;
+		node = node.getNextSibling();
+		while (/* ! (node instanceof Element) && */(node != null) && (!node.getNodeName().equals(name))) {
+			node = node.getNextSibling();
+		}
+		if (node != null) {
+			if (node.getNodeName().equals(name)) {
+				res = new NodeXML(node);
+			}
+		}
+		return res;
+	}
+
+	/**
+	 * @return the name of the node (pvdm)
+	 */
+	public String getName() {
+		return node.getNodeName();
+	}
+
+	/**
+	 * get a attribute value (pvdm)
+	 */
+	public String getAttributeValue(String attribute) {
+		if (((Element) node).getAttributeNode(attribute) == null) {
+			error("Attribute '" + attribute + "' not found on '" + node.getNodeName() + "' node.");
+		} else {
+			if (node instanceof Element) {
+				return ((Element) node).getAttributeNode(attribute).getValue();
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * get a attribute value, with a standard value if the attribute is not found.
+	 * 
+	 * @param attribute
+	 *            the name of the attribute
+	 * @param standardValue
+	 *            the standard value if attribute not found.
+	 * @return the attribute value if exist else the standard value.
+	 */
+	public String getAttributeValue(String attribute, String standardValue) {
+		String res = getAttributeValue(attribute);
+		if (res == null) {
+			res = standardValue;
+		}
+		return res;
+	}
+
+	/**
+	 * get a attribute value (pvdm)
+	 */
+	public boolean attributeExist(String attribute) {
+		return ((Element) node).getAttributeNode(attribute) != null;
+	}
+
+	/**
+	 * print error message on the screen (pvdm)
+	 */
+	protected void error(String message) {
+		if (DEBUG) {
+			System.out.println("ERROR : " + message);
+		}
+	}
+
+	/**
+	 * search a node with a specific value for a parameter (pvdm)
+	 * 
+	 * @param name
+	 *            the name of the node (pvdm)
+	 * @param paramName
+	 *            the name of the attribute of the node (pvdm)
+	 * @param paramValue
+	 *            the value of the attribute (pvdm)
+	 * @return renturn the node fund with this specification (pvdm)
+	 */
+	public NodeXML searchChild(String name, String paramName, String paramValue) {
+		NodeXML res = null;
+		NodeXML child;
+		child = getChild(name);
+		while (child != null) {
+			if (child.getAttributeValue(paramName).equals(paramValue)) {
+				res = child;
+			}
+			child = child.getNext(name);
+		}
+		return res;
+	}
+
+	public NodeXML searchChild(String inXpath) throws XPathExpressionException {
+		XPathFactory xPathfactory = XPathFactory.newInstance();
+		XPath xpath = xPathfactory.newXPath();
+		XPathExpression expr = xpath.compile(inXpath);
+		Node node = (Node) expr.evaluate(getNode(), XPathConstants.NODE);
+		return new NodeXML(node);
+	}
+
+	public String searchValue(String inXpath) throws XPathExpressionException {
+		XPathFactory xPathfactory = XPathFactory.newInstance();
+		XPath xpath = xPathfactory.newXPath();
+
+		XPathExpression expr = xpath.compile(inXpath);
+		return (String) expr.evaluate(getNode(), XPathConstants.STRING);
+	}
+
+	/**
+	 * get the first text content of the node, if any (grh)
+	 */
+	public String getContent() {
+		String res = "";
+		Node node = this.node;
+		node = node.getFirstChild();
+		while (node != null) {
+			if (node instanceof Text || node instanceof CDATASection) {
+				res = res + node.getNodeValue();
+			}
+			node = node.getNextSibling();
+
+		}
+		if (res.length() > 0) {
+			return res;
+		} else {
+			return null;
+		}
+	}
+
+	public Document getOwnerDocument() {
+		return document;
+	}
+
+	public Node getNode() {
+		return node;
+	}
+
+	public NodeXML addChild(String name) {
+		Node newNode = document.createElement(name);
+		newNode = node.appendChild(newNode);
+		return new NodeXML(newNode);
+	}
+
+	/**
+	 * Method addAttribute adds an attribute to a node with all attributes value to null, so you still have to do a setAttribute to add value.
+	 * 
+	 * @param name
+	 *            name of the new attribute.
+	 */
+	public void addAttribute(String name) {
+		Attr newAttr = document.createAttribute(name);
+		/* Node newNode = */((Element) node).setAttributeNode(newAttr);
+		// return new NodeXML(newNode);
+	}
+
+	/**
+	 * Method setAttribute sets an attrinute to its value, if the attribute does not exist it will be added. @see
+	 * 
+	 * @param name
+	 *            of the attribute.
+	 * @param value
+	 *            of the attribute.
+	 */
+	public void setAttribute(String name, String value) {
+		Element element = (Element) node;
+		element.setAttribute(name, value);
+	}
+
+	public void setContent(String content) {
+		Node newNode = node.getFirstChild();
+		if ((newNode != null) && (newNode instanceof Text)) {
+			newNode.setNodeValue(content);
+		} else {
+			newNode = document.createTextNode(content);
+			node.appendChild(newNode);
+		}
+	}
+
+	/**
+	 * setCDATAContent: Set a CDATA content in a DOM, Will add a new CDATA node if there is none. (xcosyns)
+	 * 
+	 * @param content
+	 *            The content to have as a CDATA section in your node.
+	 */
+	public void setCDATAContent(String content) {
+		Node newNode = node.getFirstChild();
+		if ((newNode != null) && (newNode instanceof CDATASection)) {
+			newNode.setNodeValue(content);
+		} else {
+			newNode = document.createCDATASection(content);
+			node.appendChild(newNode);
+		}
+	}
+
+	public void removeChild(NodeXML n) {
+		node.removeChild(n.getNode());
+	}
+
+	/**
+	 * @see java.lang.Object#clone() Clones this XMLNode with the 'deep' attribute set. So it clones this node and the whole subtree of nodes.
+	 * @author xcosyns
+	 */
+	@Override
+	public Object clone() {
+		try {
+			NodeXML clonedNodeXML = (NodeXML) super.clone();
+			// clonedNodeXML.node = this.node.cloneNode(true);
+			clonedNodeXML.document = (Document) this.document.cloneNode(true);
+			// clonedNodeXML.node = clonedNodeXML.document.importNode(this.node, true);
+			clonedNodeXML.node = clonedNodeXML.document.getDocumentElement();
+			return clonedNodeXML;
+		} catch (CloneNotSupportedException e) {
+			// this shouldn't happen because NodeXML is Cloneable
+			throw new InternalError();
+		}
+	}
+
+	public String nodeToString() {
+		StringBuffer sb = new StringBuffer("NodeXML Name = ");
+		sb.append(node.getNodeName());
+		sb.append(" NodeXML value = ");
+		sb.append(node.getNodeValue());
+		sb.append(" Content ");
+		sb.append(this.getContent());
+		sb.append("\n");
+		return sb.toString();
+	}
+
 }
