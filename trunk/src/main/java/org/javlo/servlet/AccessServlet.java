78f0314c1ff4274b9626145f51c9fbf2f6d5c0c3
pvandermaesen@noctis.be <pvandermaesen@noctis.be@7a838a29-3d20-a625-5319-021463a4e934>
2013-03-21 15:03:54 +0000

    2.0.0.18

--- trunk/src/main/java/org/javlo/servlet/AccessServlet.java
+++ trunk/src/main/java/org/javlo/servlet/AccessServlet.java
@@ -87,7 +87,7 @@ public class AccessServlet extends HttpServlet {
 	 */
 	public static Logger logger = Logger.getLogger(AccessServlet.class.getName());
 
-	public static final String VERSION = "2.0.0.17";
+	public static final String VERSION = "2.0.0.18";
 
 	@Override
 	public void destroy() {
