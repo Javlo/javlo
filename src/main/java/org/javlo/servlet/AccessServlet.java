2275ce5865e356b0669cb8ab10838e14cea55cd5
pvandermaesen@noctis.be <pvandermaesen@users.noreply.github.com>
2013-03-28 08:17:52 +0000

    2.0.2

--- src/main/java/org/javlo/servlet/AccessServlet.java
+++ src/main/java/org/javlo/servlet/AccessServlet.java
@@ -87,7 +87,7 @@ public class AccessServlet extends HttpServlet {
 	 */
 	public static Logger logger = Logger.getLogger(AccessServlet.class.getName());
 
-	public static final String VERSION = "2.0.1";
+	public static final String VERSION = "2.0.2";
 
 	@Override
 	public void destroy() {
