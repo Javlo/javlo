43510063e878a86bb8317522b637974927fd30d3
pvandermaesen@noctis.be <pvandermaesen@noctis.be@7a838a29-3d20-a625-5319-021463a4e934>
2013-06-03 16:32:40 +0000

    SmartGenericForm don't inherited from GenericForm

--- trunk/src/main/java/org/javlo/component/form/SmartGenericForm.java
+++ trunk/src/main/java/org/javlo/component/form/SmartGenericForm.java
@@ -1,27 +1,58 @@
 package org.javlo.component.form;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.logging.Logger;
 
+import javax.mail.internet.InternetAddress;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.lang.StringUtils;
+import org.javlo.actions.IAction;
+import org.javlo.component.core.AbstractVisualComponent;
+import org.javlo.config.StaticConfig;
 import org.javlo.context.ContentContext;
+import org.javlo.context.GlobalContext;
 import org.javlo.helper.LangHelper;
+import org.javlo.helper.ResourceHelper;
 import org.javlo.helper.StringHelper;
+import org.javlo.helper.URLHelper;
 import org.javlo.helper.XHTMLHelper;
 import org.javlo.helper.Comparator.StringComparator;
+import org.javlo.mailing.MailService;
+import org.javlo.message.GenericMessage;
+import org.javlo.service.CaptchaService;
+import org.javlo.service.ContentService;
 import org.javlo.service.RequestService;
+import org.javlo.utils.CSVFactory;
+import org.javlo.utils.CollectionAsMap;
+import org.javlo.ztatic.StaticInfo;
 
-public class SmartGenericForm extends GenericForm {
+public class SmartGenericForm extends AbstractVisualComponent implements IAction{
+	
+	private Properties bundle;
+	
+	private static Logger logger = Logger.getLogger(SmartGenericForm.class.getName());
+	
+	protected static final Object LOCK = new Object();
 
 	public static class Field {
 
@@ -148,6 +179,40 @@ public class SmartGenericForm extends GenericForm {
 			this.registeredList = registeredList;
 		}
 	}
+	
+	public Properties getLocalConfig(boolean reload) {
+		if (bundle == null || reload) {
+			bundle = new Properties();
+			try {
+				bundle.load(new StringReader(getValue()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return bundle;
+	}
+	
+	protected File getAttachFolder(ContentContext ctx) throws IOException {
+		GlobalContext globalContext = GlobalContext.getInstance(ctx.getRequest());
+		String fileName = "df-" + getId();
+		if (getLocalConfig(false).get("filename") != null) {
+			fileName = getLocalConfig(false).getProperty("filename");
+		}
+		fileName = StringHelper.getFileNameWithoutExtension(fileName);
+		File dir = new File(URLHelper.mergePath(globalContext.getDataFolder(), globalContext.getStaticConfig().getStaticFolder(), "dynamic-form-result", fileName));
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+		return dir;
+	}
+	
+	protected boolean isCaptcha(ContentContext ctx) {
+		return StringHelper.isTrue(getLocalConfig(false).getProperty("captcha", "" + isCaptcha()));
+	}
+	
+	public boolean isCaptcha() {
+		return StringHelper.isTrue(getLocalConfig(false).getProperty("captcha", "true"));
+	}
 
 	public static final String TYPE = "smart-generic-form";
 
@@ -186,7 +251,7 @@ public class SmartGenericForm extends GenericForm {
 			if (isList()) {
 				listTitle = "<td>list</td>";
 			}
-			out.println("<thead><tr><td>name</td><td>label</td>" + listTitle + "<td>type</td><td>require</td><td>action</td></tr></thead>");
+			out.println("<thead><tr><td>name</td><td>label</td>" + listTitle + "<td>type</td><td>required</td><td>action</td></tr></thead>");
 			out.println("<tbody>");
 			List<Field> fields = getFields();
 			for (Field field : fields) {
@@ -373,9 +438,281 @@ public class SmartGenericForm extends GenericForm {
 
 		store(ctx);
 	}
+	
+	protected String getMailHeader(ContentContext ctx) {
+		return "";
+	}
+
+	protected String getMailFooter(ContentContext ctx) {
+		return "";
+	}
+
+	protected boolean isHTMLMail() {
+		return false;
+	}
+	
+	protected boolean isStorage() {
+		return true;
+	}
+	
+	protected File getFile(ContentContext ctx) throws IOException {
+		GlobalContext globalContext = GlobalContext.getInstance(ctx.getRequest());
+		String fileName = "df-" + getId() + ".csv";
+		if (getLocalConfig(false).get("filename") != null && getLocalConfig(false).get("filename").toString().trim().length() > 0) {
+			fileName = getLocalConfig(false).getProperty("filename");
+		}
+		File file = new File(URLHelper.mergePath(globalContext.getDataFolder(), globalContext.getStaticConfig().getStaticFolder(), "dynamic-form-result", fileName));
+		if (!file.exists()) {
+			file.getParentFile().mkdirs();
+			file.createNewFile();
+		}
+		return file;
+	}
+	
+	protected void storeResult(ContentContext ctx, Map<String, String> data) throws IOException {
+
+		synchronized (LOCK) {
+			File file = getFile(ctx);
+			Collection<String> titles = CSVFactory.loadTitle(file);
+			boolean newTitleFound = false;
+			for (String newTitle : data.keySet()) {
+				if (!titles.contains(newTitle)) {
+					newTitleFound = true;
+				}
+			}
+			if (newTitleFound) {
+				List<Map<String, String>> newData = CSVFactory.loadContentAsMap(file);
+				newData.add(data);
+				CSVFactory.storeContentAsMap(file, newData);
+			} else {
+				CSVFactory.appendContentAsMap(file, data);
+			}
+		}
+	}
+	
+	protected boolean isSendEmail() {
+		return true;
+	}
+
+	
+	public static String performSubmit(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		RequestService requestService = RequestService.getInstance(request);
+		ContentContext ctx = ContentContext.getContentContext(request, response);
+		ContentService content = ContentService.getInstance(request);
+		SmartGenericForm comp = (SmartGenericForm) content.getComponent(ctx, requestService.getParameter("comp_id", null));
+
+		/** check captcha **/
+		String captcha = requestService.getParameter("captcha", null);
+
+		if (comp.isCaptcha(ctx)) {
+			if (captcha == null || CaptchaService.getInstance(request.getSession()).getCurrentCaptchaCode() == null || !CaptchaService.getInstance(request.getSession()).getCurrentCaptchaCode().equals(captcha)) {
+				GenericMessage msg = new GenericMessage(comp.getLocalConfig(false).getProperty("error.captcha", "bad captcha."), GenericMessage.ERROR);
+
+				request.setAttribute("msg", msg);
+				request.setAttribute("error_captcha", "true");
+				return null;
+			} else {
+				CaptchaService.getInstance(request.getSession()).setCurrentCaptchaCode("");
+			}
+		}
+
+		GlobalContext globalContext = GlobalContext.getInstance(ctx.getRequest());
+
+		String subject = "GenericForm submit : '" + globalContext.getGlobalTitle() + "' ";
+
+		String subjectField = comp.getLocalConfig(false).getProperty("mail.subject.field", null);
+		if (subjectField != null && requestService.getParameter(subjectField, null) != null) {
+			subject = comp.getLocalConfig(false).getProperty("mail.subject", "") + requestService.getParameter(subjectField, null);
+		} else {
+			subject = comp.getLocalConfig(false).getProperty("mail.subject", subject);
+		}
+
+		Map<String, Object> params = requestService.getParameterMap();
+		Map<String, String> result = new HashMap<String, String>();
+		List<String> errorFields = new LinkedList<String>();
+		result.put("__registration time", StringHelper.renderSortableTime(new Date()));
+		result.put("__local addr", request.getLocalAddr());
+		result.put("__remote addr", request.getRemoteAddr());
+		result.put("__X-Forwarded-For", request.getHeader("x-forwarded-for"));
+		result.put("__X-Real-IP", request.getHeader("x-real-ip"));
+		result.put("__referer", request.getHeader("referer"));
+
+		String fakeField = comp.getLocalConfig(false).getProperty("field.fake", "fake");
+		boolean withXHTML = StringHelper.isTrue(comp.getLocalConfig(false).getProperty("field.xhtml", null));
+		boolean fakeFilled = false;
+
+		List<String> keys = new LinkedList<String>(params.keySet());
+		Collections.sort(keys, new StringComparator());
+
+		/** store attach files **/
+
+		Map<String, String> specialValues = new HashMap<String, String>();
+
+		String badFileFormatRAW = comp.getLocalConfig(false).getProperty("file.bad-file", "exe,bat,scr,bin,obj,lib,dll,bat,sh,com,cmd,msi,jsp,xml,html,htm,vbe,wsf,wsc,asp");
+		List<String> badFileFormat = StringHelper.stringToCollection(badFileFormatRAW, ",");
+		String maxFileSizeRAW = comp.getLocalConfig(false).getProperty("file.max-size", "" + (10 * 1024 * 1024));
+		long maxFileSize = Long.parseLong(maxFileSizeRAW);
+
+		for (FileItem file : requestService.getAllFileItem()) {
+			String ext = StringHelper.getFileExtension(file.getName()).toLowerCase();
+			if (badFileFormat.contains(ext)) {
+				logger.warning("file blocked because bad extenstion : " + file.getName());
+				GenericMessage msg = new GenericMessage(comp.getLocalConfig(false).getProperty("message.bad-file", "bad file format."), GenericMessage.ERROR);
+				request.setAttribute("msg", msg);
+				return null;
+			}
+			InputStream in = file.getInputStream();
+			if (in != null) {
+				try {
+					if (file.getName().trim().length() > 0) {
+						String fileName = URLHelper.mergePath(comp.getAttachFolder(ctx).getAbsolutePath(), StringHelper.createFileName(file.getName()));
+						File freeFile = ResourceHelper.getFreeFileName(new File(fileName));
+						if (ResourceHelper.writeStreamToFile(in, freeFile, maxFileSize) < 0) {
+							GenericMessage msg = new GenericMessage(comp.getLocalConfig(false).getProperty("message.tobig-file", "file to big."), GenericMessage.ERROR);
+							request.setAttribute("msg", msg);
+							return null;
+						}
+						StaticInfo staticInfo = StaticInfo.getInstance(ctx, freeFile);
+						String fileURL = URLHelper.createResourceURL(ctx.getContextForAbsoluteURL(), URLHelper.mergePath(globalContext.getStaticConfig().getStaticFolder(), staticInfo.getStaticURL()));
+						result.put(file.getFieldName(), fileURL);
+						specialValues.put(file.getFieldName(), fileURL);
+					}
+				} finally {
+					ResourceHelper.closeResource(in);
+				}
+			}
+		}
+
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		PrintStream out = new PrintStream(outStream);
+
+		for (Field field : comp.getFields()) {
+			String key = field.getName();
+			
+			Object value = params.get(key);
+			if (specialValues.get(key) != null) {
+				value = specialValues.get(key);
+			}
+			String finalValue = requestService.getParameter(key, "");
+			if (specialValues.get(key) != null) {
+				finalValue = specialValues.get(key);
+			}
+
+			if (key.equals(fakeField) && finalValue.trim().length() > 0) {
+				fakeFilled = true;
+			} else if (!withXHTML && (finalValue.toLowerCase().contains("</a>") || finalValue.toLowerCase().contains("</div>"))) {
+				fakeFilled = true;
+			}
+
+			if (finalValue.trim().length() == 0 && StringHelper.containsUppercase(key)) { // needed field
+				errorFields.add(key);
+				GenericMessage msg = new GenericMessage(comp.getLocalConfig(false).getProperty("error.required", "please could you fill all required fields."), GenericMessage.ERROR);
+				request.setAttribute("msg", msg);
+			}
+
+			if (value instanceof Object[]) {
+				finalValue = StringHelper.arrayToString((Object[]) params.get(key), ",");
+				out.println(field.getLabel()+ " ("+key+") " + ':');
+				out.println(finalValue);
+			} else {
+				out.println(field.getLabel()+ " ("+key+") " + ':');
+				out.println(finalValue);
+			}
+			out.println("");
+			result.put(key, finalValue);
+		
+		}
+		out.println("");
+		out.close();
+
+		if (fakeFilled) {
+			logger.warning("spam detected fake field filled : " + comp.getPage().getPath());
+		}
+		
+
+		
+
+		if (errorFields.size() == 0) {
+			String mailContent = new String(outStream.toByteArray());
+			if (comp.isHTMLMail()) {
+				mailContent = XHTMLHelper.textToXHTML(mailContent);
+			}
+			mailContent = comp.getMailHeader(ctx) + mailContent + comp.getMailFooter(ctx);
+
+			logger.info("mail content : " + mailContent);
+
+			if (comp.isStorage()) {
+				comp.storeResult(ctx, result);
+			}
+
+			if (comp.isSendEmail() && !fakeFilled) {
+
+				String emailFrom = comp.getLocalConfig(false).getProperty("mail.from", StaticConfig.getInstance(request.getSession()).getSiteEmail());
+				String emailFromField = comp.getLocalConfig(false).getProperty("mail.from.field", null);
+				if (emailFromField != null && requestService.getParameter(emailFromField, "") != null) {
+					String tmpEmail = requestService.getParameter(emailFromField, "");
+					try {
+						new InternetAddress(tmpEmail);
+						emailFrom = tmpEmail;
+					} catch (Exception e) {
+						logger.warning(e.getMessage());
+					}
+				}
+				String emailTo = comp.getLocalConfig(false).getProperty("mail.to", globalContext.getAdministratorEmail());
+				String emailCC = comp.getLocalConfig(false).getProperty("mail.cc", null);
+				String emailBCC = comp.getLocalConfig(false).getProperty("mail.bcc", null);
+
+				try {
+
+					MailService mailService = MailService.getInstance(globalContext.getStaticConfig());
+					InternetAddress fromEmail = new InternetAddress(emailFrom);
+					InternetAddress toEmail = new InternetAddress(emailTo);
+					InternetAddress ccEmail = null;
+					if (emailCC != null && emailCC.trim().length() > 0) {
+						ccEmail = new InternetAddress(emailCC);
+					}
+					InternetAddress bccEmail = null;
+					if (emailBCC != null && emailBCC.trim().length() > 0) {
+						bccEmail = new InternetAddress(emailBCC);
+					}
+
+					List<InternetAddress> ccList = null;
+					if (ccEmail != null) {
+						ccList = Arrays.asList(ccEmail);
+					}
+					List<InternetAddress> bccList = null;
+					if (bccEmail != null) {
+						bccList = Arrays.asList(bccEmail);
+					}
+
+					mailService.sendMail(null, fromEmail, toEmail, ccList, bccList, subject, mailContent, comp.isHTMLMail());
+				} catch (Exception e) {
+					e.printStackTrace();
+					GenericMessage msg = new GenericMessage(comp.getLocalConfig(false).getProperty("message.error", "technical error."), GenericMessage.ERROR);
+					request.setAttribute("msg", msg);
+					request.setAttribute("valid", "false");
+					return null;
+				}
+			}
+
+			GenericMessage msg = new GenericMessage(comp.getLocalConfig(false).getProperty("message.thanks"), GenericMessage.INFO);
+			request.setAttribute("msg", msg);
+			request.setAttribute("valid", "true");
+		} else {
+			request.setAttribute("errorFields", new CollectionAsMap<String>(errorFields));
+		}
+
+		return null;
+	}
 
 	@Override
 	public String getType() {
 		return TYPE;
+	}
+
+	@Override
+	public String getActionGroupName() {
+		return "smart-form";
 	}
 }
