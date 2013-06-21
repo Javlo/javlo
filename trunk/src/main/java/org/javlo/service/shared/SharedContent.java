cc7f0eaa7ad0401915c62a8d3faf28d7c49fcbb6
pvandermaesen@noctis.be <pvandermaesen@noctis.be@7a838a29-3d20-a625-5319-021463a4e934>
2013-06-21 20:56:39 +0000

    shared content

---
+++ trunk/src/main/java/org/javlo/service/shared/SharedContent.java
@@ -0,0 +1,60 @@
+package org.javlo.service.shared;
+
+import java.util.List;
+
+import org.javlo.component.core.ComponentBean;
+import org.javlo.component.image.GlobalImage;
+import org.javlo.component.text.Description;
+import org.javlo.component.title.Title;
+import org.javlo.context.ContentContext;
+
+public class SharedContent {
+	
+	private String title = null;
+	private String description = null;
+	private String imageURL = null;
+	private String id = null;
+	
+	private final List<ComponentBean> content;
+	
+	public SharedContent (ContentContext ctx, String id, List<ComponentBean> content) {
+		this.id = id;
+		this.content = content;		
+		for (ComponentBean componentBean : content) {
+			if (componentBean.getType().equals(Title.TYPE)) {
+				title = componentBean.getValue();
+			} else if (componentBean.getType().equals(Description.TYPE)) {
+				description = componentBean.getValue();
+			} else if (componentBean.getType().equals(GlobalImage.TYPE)) {								
+				try {
+					GlobalImage image = new GlobalImage();
+					image.init(componentBean, ctx);
+					imageURL = image.getPreviewURL(ctx, "shared-preview");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public List<ComponentBean> getContent() {
+		return content;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getImageURL() {
+		return imageURL;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+}
---
+++ trunk/src/main/webapp/jsp/preview/shared_content.jsp
@@ -0,0 +1,13 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<div class="content shared-content">
+<c:forEach var="content" items="${sharedContent}">
+<div class="content-wrapper">
+<div class="content" data-shared="${content.id}">
+	<h4>${content.title}</h4>
+	<figure><img src="${content.imageURL}" /></figure>
+	<div class="description">${content.description}</div>	
+</div>
+</div>
+</c:forEach>
+</div>
