6d987f09c45c60313d73550cdabbe82119d315ef
pvandermaesen@noctis.be <pvandermaesen@noctis.be@7a838a29-3d20-a625-5319-021463a4e934>
2013-09-13 22:50:32 +0000

    add trace

--- trunk/src/main/java/org/javlo/servlet/ImageTransformServlet.java
+++ trunk/src/main/java/org/javlo/servlet/ImageTransformServlet.java
@@ -593,7 +593,9 @@ public class ImageTransformServlet extends HttpServlet {
 				/* last modified management */
 				long lastModified = getLastModified(globalContext, imageName, filter, area, ctx.getDevice(), template);
 				response.setDateHeader(NetHelper.HEADER_LAST_MODIFIED, lastModified);
-				long lastModifiedInBrowser = request.getDateHeader(NetHelper.HEADER_IF_MODIFIED_SINCE);				
+				long lastModifiedInBrowser = request.getDateHeader(NetHelper.HEADER_IF_MODIFIED_SINCE);
+				System.out.println("***** ImageTransformServlet.processRequest : lastModified = "+lastModified); //TODO: remove debug trace
+				System.out.println("***** ImageTransformServlet.processRequest : lastModifiedInBrowser = "+lastModifiedInBrowser); //TODO: remove debug trace
 				if (lastModified > 0 && lastModified/1000 <= lastModifiedInBrowser/1000) {					
 					response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
 					return;
