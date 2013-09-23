6fe86901e21e56ba340b61964d4a4f90655f1eed
pvandermaesen@noctis.be <pvandermaesen@users.noreply.github.com>
2013-09-23 19:25:30 +0000

    leave access to registration page.

--- src/main/java/org/javlo/servlet/AccessServlet.java
+++ src/main/java/org/javlo/servlet/AccessServlet.java
@@ -674,7 +674,7 @@ public class AccessServlet extends HttpServlet implements IVersion {
 							
 							if (ctx.getGlobalContext().isCollaborativeMode()) {
 								Set<String> pageRoles = ctx.getCurrentPage().getEditorRoles();
-								if (pageRoles.size() > 0 || ctx.getCurrentEditUser() == null) {
+								if ((pageRoles.size() > 0 || ctx.getCurrentEditUser() == null) && !ctx.getCurrentPage().getName().equals("registration")) { // leave access to registration page.
 									if (ctx.getCurrentEditUser() == null || !ctx.getCurrentEditUser().validForRoles(pageRoles)) {
 										ctx.setSpecialContentRenderer("/jsp/view/no_access.jsp");
 									}
