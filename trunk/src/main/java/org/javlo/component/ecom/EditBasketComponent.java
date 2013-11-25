97efc48254d314474565fda83dac0fd2c45a073a
pvandermaesen@noctis.be <pvandermaesen@noctis.be@7a838a29-3d20-a625-5319-021463a4e934>
2013-11-25 17:24:18 +0000

    company management

--- trunk/src/main/java/org/javlo/component/ecom/EditBasketComponent.java
+++ trunk/src/main/java/org/javlo/component/ecom/EditBasketComponent.java
@@ -99,7 +99,7 @@ public class EditBasketComponent extends AbstractVisualComponent implements IAct
 		String city = rs.getParameter("city", "").trim();
 		
 		String vta = rs.getParameter("vat", "").trim();
-		String company = rs.getParameter("company", "").trim();
+		String company = rs.getParameter("organization", "").trim();
 
 		if (firstName.length() == 0 || lastName.length() == 0 || email.length() == 0 || country.length() == 0 || address.length() == 0 || zip.length() == 0 || city.length() == 0) {
 			messageRepository.setGlobalMessage(new GenericMessage(i18nAccess.getViewText("global.compulsory-field"), GenericMessage.ERROR));
