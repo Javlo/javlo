15253752a88be32cc3b8b7a73cc30e3c47ce2463
pvandermaesen@noctis.be <pvandermaesen@noctis.be@7a838a29-3d20-a625-5319-021463a4e934>
2014-04-24 16:09:34 +0000

    template optimisation

--- trunk/src/main/java/org/javlo/template/Row.java
+++ trunk/src/main/java/org/javlo/template/Row.java
@@ -4,41 +4,30 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Row extends TemplatePart {
-	
+
 	private Template template = null;
-	
+
 	private List<Area> areas = new LinkedList<Area>();
-	
+
 	public Row(Template inTemplate) {
 		template = inTemplate;
 	}
-	
+
 	public List<Area> getAreas() {
 		return areas;
 	}
-	
+
 	public void addArea(Area area) {
 		areas.add(area);
 	}
-	
+
 	@Override
-	public String getFont() {	
-		String font = super.getFont();
-		if (font == null || font.trim().length() == 0 && template != null) {
-			return template.getStyle().getFont();
+	protected TemplatePart getParent() {
+		if (template != null) {
+			return template.getStyle();
 		} else {
-			return font;
+			return null;
 		}
 	}
-	
-	@Override
-	public String getTextSize() {	
-		String textSize = super.getTextSize();
-		if (textSize == null || textSize.trim().length() == 0 && template != null) {
-			return template.getStyle().getTextSize();
-		} else {
-			return textSize;
-		}
-	}	
 
 }
