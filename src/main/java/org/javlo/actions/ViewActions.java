5da563aec5754d342d346ff56621c3f2a195df83
pvandermaesen@noctis.be <pvandermaesen@users.noreply.github.com>
2014-05-26 12:11:41 +0000

    change language correction

--- src/main/java/org/javlo/actions/ViewActions.java
+++ src/main/java/org/javlo/actions/ViewActions.java
@@ -45,23 +45,24 @@ public class ViewActions implements IAction {
 		try {
 			ContentContext ctx = ContentContext.getContentContext(request, response);
 			GlobalContext globalContext = GlobalContext.getInstance(request);
-			String lang = request.getParameter("lg");
+			String lang = request.getParameter("lg");			
 			if (lang != null) {
 				if (globalContext.getLanguages().contains(lang)) {
-					System.out.println("***** ViewActions.performLanguage : 1.path = "+ctx.getPath()); //TODO: remove debug trace
-					System.out.println("***** ViewActions.performLanguage : 1.current Page = "+ctx.getCurrentPage().getPath()); //TODO: remove debug trace
 					ctx.setAllLanguage(lang);
 					ctx.setCookieLanguage(lang);
 					I18nAccess i18nAccess = I18nAccess.getInstance(globalContext, request.getSession());
 					i18nAccess.changeViewLanguage(ctx);
 					String url = URLHelper.createURL(ctx);
-					System.out.println("***** ViewActions.performLanguage : 2.path = "+ctx.getPath()); //TODO: remove debug trace
-					System.out.println("***** ViewActions.performLanguage : 2.current Page = "+ctx.getCurrentPage().getPath()); //TODO: remove debug trace
-					System.out.println("***** ViewActions.performLanguage : 2.url = "+url); //TODO: remove debug trace
 					NetHelper.sendRedirectTemporarily(response, url);
+				} else if (globalContext.getContentLanguages().contains(lang)) {
+					ctx.setContentLanguage(lang);
+					ctx.setCookieLanguage(lang);
+					I18nAccess i18nAccess = I18nAccess.getInstance(globalContext, request.getSession());
+					i18nAccess.changeViewLanguage(ctx);
+					String url = URLHelper.createURL(ctx);
+					NetHelper.sendRedirectTemporarily(response, url);					
 				}
 			}
-
 		} catch (Exception e) {
 			msg = e.getMessage();
 		}
