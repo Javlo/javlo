e8818a1473e0a63904836c1bc25865aa285e8233
pvandermaesen@noctis.be <pvandermaesen@users.noreply.github.com>
2014-06-15 15:36:49 +0000

    simple poll with only one response.

---
+++ src/main/java/org/javlo/helper/PrefixHeadContext.java
@@ -0,0 +1,21 @@
+package org.javlo.helper;
+
+public class PrefixHeadContext {
+	
+	private boolean description = true;
+	private boolean keyword = true;
+	
+	public boolean isDescription() {
+		return description;
+	}
+	public void setDescription(boolean description) {
+		this.description = description;
+	}
+	public boolean isKeyword() {
+		return keyword;
+	}
+	public void setKeyword(boolean keyword) {
+		this.keyword = keyword;
+	}	
+
+}
