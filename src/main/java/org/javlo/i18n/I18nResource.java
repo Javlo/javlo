7ff2494ac57374df44258e1f5ce3ba4bb51dcd3c
pvandermaesen@noctis.be <pvandermaesen@users.noreply.github.com>
2014-06-24 14:00:48 +0000

    clean import

--- src/main/java/org/javlo/i18n/I18nResource.java
+++ src/main/java/org/javlo/i18n/I18nResource.java
@@ -2,7 +2,6 @@ package org.javlo.i18n;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
