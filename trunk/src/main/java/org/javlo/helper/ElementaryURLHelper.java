42b4cdfdf22508c0e20ca05acfb36b9731dfa603
pvandermaesen@noctis.be <pvandermaesen@noctis.be@7a838a29-3d20-a625-5319-021463a4e934>
2014-09-10 10:54:54 +0000

    imageTransform function sequence correction

--- trunk/src/main/java/org/javlo/helper/ElementaryURLHelper.java
+++ trunk/src/main/java/org/javlo/helper/ElementaryURLHelper.java
@@ -395,7 +395,7 @@ public abstract class ElementaryURLHelper {
 	}
 
 	public static String createTransformURL(ContentContext ctx, MenuElement referencePage, String url, String filter) throws Exception {
-		return createTransformURL(ctx, referencePage, ctx.getCurrentTemplate(), url, filter, null);
+		return createTransformURLInternal(ctx, referencePage, url, filter, ctx.getCurrentTemplate().getName(),null,null);
 	}
 
 	public static String createTransformURL(ContentContext ctx, MenuElement referencePage, String url, String filter, String templateName) throws Exception {
