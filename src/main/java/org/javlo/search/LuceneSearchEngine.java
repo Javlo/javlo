c20f294db4ed04143a2457391cece5c29bd51058
bdumont@noctis.be <bdumontdech@users.noreply.github.com>
2014-12-08 15:26:06 +0000

    Enhance Lucene integration

--- src/main/java/org/javlo/search/LuceneSearchEngine.java
+++ src/main/java/org/javlo/search/LuceneSearchEngine.java
@@ -4,11 +4,12 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.StoredField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
@@ -25,8 +26,12 @@ import org.apache.lucene.util.Version;
 import org.javlo.component.core.ContentElementList;
 import org.javlo.component.core.IContentVisualComponent;
 import org.javlo.context.ContentContext;
+import org.javlo.helper.ResourceHelper;
 import org.javlo.helper.StringHelper;
 import org.javlo.helper.URLHelper;
+import org.javlo.i18n.I18nAccess;
+import org.javlo.message.GenericMessage;
+import org.javlo.message.MessageRepository;
 import org.javlo.navigation.MenuElement;
 import org.javlo.search.SearchResult.SearchElement;
 import org.javlo.service.ContentService;
@@ -38,7 +43,13 @@ import org.javlo.service.ContentService;
  */
 public class LuceneSearchEngine implements ISearchEngine {
 
-	private static final String ID_FIELD = "_ID";
+	private static final String ID_FIELD = "_id";
+	private static final String TYPE_FIELD = "_type";
+	private static final String LEVEL_FIELD_PREFIX = "level";
+	private static final String PAGE_TYPE = "page";
+
+	private static final Pattern ESCAPER = Pattern.compile("([ +\\-!\\(\\)\\{\\}\\[\\\\\\]^\\\"~\\*\\?\\:]|&&|\\|\\|)");
+	private static final String ESCAPER_REPLACE = "\\\\$0";
 
 	private Directory index;
 	private StandardAnalyzer analyzer;
@@ -47,9 +58,16 @@ public class LuceneSearchEngine implements ISearchEngine {
 	public List<SearchElement> search(ContentContext ctx, String groupId, String searchStr, String sort, List<String> componentList) throws Exception {
 		init(ctx);
 
-		Query q = new QueryParser("title", analyzer).parse(searchStr);
+		String queryPattern = "level3:{QUERY}^3 level2:{QUERY}^2 level1:{QUERY}^1";//TODO move this to static config
 
-		int hitsPerPage = 10;
+		String escapedStr = escapeLucene(searchStr.trim());
+
+		String queryStr = queryPattern.replace("{QUERY}", escapedStr);
+		System.out.println(queryStr);
+
+		Query q = new QueryParser("level3", analyzer).parse(queryStr);
+
+		int hitsPerPage = 5000;
 		IndexReader reader = DirectoryReader.open(index);
 		IndexSearcher searcher = new IndexSearcher(reader);
 		TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
@@ -69,8 +87,17 @@ public class LuceneSearchEngine implements ISearchEngine {
 			MenuElement page = nav.searchChildFromId(pageId);
 			addResult(result, ctx, page, searchStr, page.getName(), page.getFullLabel(ctxWithContent), URLHelper.createURL(ctxWithContent, page.getPath()), page.getDescription(ctxWithContent), 1);
 		}
+		if (result.size() == 0) {
+			I18nAccess i18nAccess = I18nAccess.getInstance(ctx.getRequest());
+			MessageRepository messageRepository = MessageRepository.getInstance(ctx);
+			messageRepository.setGlobalMessage(new GenericMessage(i18nAccess.getText("search.title.no-result") + ' ' + searchStr, GenericMessage.ALERT));
+		}
 
 		return result;
+	}
+
+	private static String escapeLucene(String searchStr) {
+		return ESCAPER.matcher(searchStr).replaceAll(ESCAPER_REPLACE);
 	}
 
 	private void addResult(List<SearchElement> result, ContentContext ctx, MenuElement page, String searchElement, String name, String title, String url, String description, int priority) {
@@ -104,71 +131,50 @@ public class LuceneSearchEngine implements ISearchEngine {
 		} catch (Exception e) {
 			//logger.warning(e.getMessage());
 		}
-//		if (priority > maxPriority) {
-//			maxPriority = priority;
-//		}
 		result.add(rst);
 	}
 
 	private void init(ContentContext ctx) throws Exception {
-//		if (index != null) {
-//			return;
-//		}
+		if (index != null) {
+			return;
+		}
 
 		analyzer = new StandardAnalyzer();
 		index = new RAMDirectory();
 
-		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_2, analyzer);//TODO try finally to close?
-		IndexWriter w = new IndexWriter(index, config);
+		//TODO index by language
+		//TODO move index to site scope (not session scope)
 
 		ContentService content = ContentService.getInstance(ctx.getRequest());
 		MenuElement nav = content.getNavigation(ctx);
 
-		indexPage(nav, ctx, w);
-
-//		Iterator<SearchElement> results = result.iterator();
-//		while (results.hasNext()) {
-//			SearchElement element = results.next();
-//			element.setMaxPriority(maxPriority);
-//		}
-//		addResult(ctx, page, inSearchText, page.getName(), page.getFullLabel(ctxWithContent), URLHelper.createURL(ctxWithContent, page.getPath()), page.getDescription(ctxWithContent), searchLevel, componentsRenderer);
-//		if (result.size() == 0) {
-//			I18nAccess i18nAccess = I18nAccess.getInstance(ctx.getRequest());
-//			MessageRepository messageRepository = MessageRepository.getInstance(ctx);
-//			messageRepository.setGlobalMessage(new GenericMessage(i18nAccess.getText("search.title.no-result") + ' ' + searchText, GenericMessage.ALERT));
-//		}
-		w.close();
-
+		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_2, analyzer);
+		IndexWriter w = null;
+		try {
+			w = new IndexWriter(index, config);
+			indexPage(nav, ctx, w);
+		} finally {
+			ResourceHelper.safeClose(w);
+		}
 	}
-//	private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
-//		Document doc = new Document();
-//		doc.add(new TextField("title", title, Field.Store.YES));
-//		doc.add(new StringField("isbn", isbn, Field.Store.YES));
-//		w.addDocument(doc);
-//	}
 
 	private void indexPage(MenuElement page, ContentContext ctx, IndexWriter w) throws Exception {
 
 		if (!page.notInSearch(ctx)) {
 			ContentContext ctxWithContent = ctx.getContextWithContent(page);
 			if (ctxWithContent != null) {
-
 				Document doc = new Document();
-				doc.add(new StringField(ID_FIELD, page.getId(), Field.Store.YES));
-				doc.add(new StringField("name", page.getName(), Field.Store.YES));
-				doc.add(new TextField("title", page.getTitle(ctx), Field.Store.YES));
-				doc.add(new TextField("description", page.getDescription(ctx), Field.Store.YES));
-				//TODO page.getGroupID(ctx);
+				doc.add(new StoredField(ID_FIELD, page.getId()));
+				doc.add(new StoredField(TYPE_FIELD, PAGE_TYPE));
+
+				//Index page groups? page.getGroupID(ctx)
 
 				ContentElementList elemList = page.getLocalContentCopy(ctxWithContent);
-
-				while (elemList.hasNext(ctxWithContent)) {
-					IContentVisualComponent cpt = elemList.next(ctxWithContent);
-
+				for (IContentVisualComponent cpt : elemList.getIterable(ctxWithContent)) {
 					if (cpt.getSearchLevel() > 0) {
-						//TODO cpt.getSearchLevel();
-						TextField fld = new TextField("comp_" + cpt.getId(), cpt.getTextForSearch(), Field.Store.NO);
-						doc.add(fld);
+						doc.add(new TextField(LEVEL_FIELD_PREFIX + cpt.getSearchLevel(), cpt.getTextForSearch(), Field.Store.NO));
+						//Automatic boost? (currently done via StaticConfig)
+						//field.setBoost(1F + ((float) cpt.getSearchLevel()) / 10F);
 					}
 				}
 
