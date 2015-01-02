9ae9f5ff1caf77a4d36bf387edda1f71f8c1680d
pvandermaesen@noctis.be <pvandermaesen@users.noreply.github.com>
2015-01-02 20:14:55 +0000

    edit user bootstrap layout + sort children macro

---
+++ src/main/java/org/javlo/macro/SortChildren.java
@@ -0,0 +1,46 @@
+package org.javlo.macro;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.javlo.context.ContentContext;
+import org.javlo.context.GlobalContext;
+import org.javlo.navigation.MenuElement;
+import org.javlo.navigation.MenuElementNameComparator;
+import org.javlo.service.PersistenceService;
+
+public class SortChildren extends AbstractMacro {
+
+	@Override
+	public String getName() {
+		return "sort-children";
+	}
+
+	@Override
+	public String perform(ContentContext ctx, Map<String, Object> params) throws Exception {
+		GlobalContext globalContext = GlobalContext.getInstance(ctx.getRequest());
+		
+		List<MenuElement> mustSorted = new LinkedList<MenuElement>(ctx.getCurrentPage().getChildMenuElements());
+		
+		Collections.sort(mustSorted, new MenuElementNameComparator(ctx, true));		
+		int i=0;
+		for (MenuElement menuElement : mustSorted) {
+			i++;
+			menuElement.setPriority(10*i);
+		}
+		mustSorted=null;
+
+		PersistenceService persistenceService = PersistenceService.getInstance(globalContext);
+		persistenceService.store(ctx);
+
+		return null;
+	}
+
+	@Override
+	public boolean isPreview() {
+		return true;
+	}
+
+};
--- src/main/java/org/javlo/macro/core/MacroFactory.java
+++ src/main/java/org/javlo/macro/core/MacroFactory.java
@@ -54,6 +54,7 @@ import org.javlo.macro.NoClickableImageMacro;
 import org.javlo.macro.PasteCopiedElementInAllLanguageMacro;
 import org.javlo.macro.ReduceSubtitleLevelMacro;
 import org.javlo.macro.SmartImport;
+import org.javlo.macro.SortChildren;
 import org.javlo.macro.UpDateMacro;
 import org.javlo.macro.UploadGallery;
 import org.javlo.macro.deleteComponentInBadArea;
@@ -63,7 +64,7 @@ public class MacroFactory {
 	private final List<IMacro> macros = new LinkedList<IMacro>();
 
 	private static final List<IMacro> defaultMacros = Arrays.asList(new IMacro[] { new CreateContentChildren(), new DeleteDynamicComponent(), new AddChildMacro(), new CreateChildren(), new ImportContent(), new SmartImport(), new CreateArticle(), new CreateAlphabeticChildrenHereMacro(), new CreateArticleComposition(), new ImportHTMLPageMacro(), new ImportJCRPageMacro(), new ImportGalleryMacro(), new CreateDefaultPageStructure(), new CreatePressReleaseTodayMacro(), new CreatePressReleaseHereMacro(), new CreateExternalNewsMacro(), new CreateWeekHereMacro(), new CreateMonthHereMacro(), new CreatePressReleaseTodayHereMacro(), new CreateMonthStructureMacro(), new CreateBaseStructureMacro(), new CreateChildrenStructureMacro(), new DeletePageFromSpecificUser(), new MergeDynamicComponent(), new ReduceSubtitleLevelMacro(), new IncreaseSubtitleLevelMacro(), new CleanResourceNameMacro(), new CopyLanguageStructureMacro(), new CopyLanguageStructureHereMacro(), new ImportDefaultLanguageMacro(), new DeleteSmartExternalLinkMacro(), new ImageAfterDescriptionMacro(), new MacroRendererCorrection(), new ImageAfterDescriptionChildrenMacro(), new NoClickableImageMacro(),
-			new CurrentLgExpandMacro(), new EncryptPasswordComponent(), new EncryptVisitorsPasswordMacro(), new CreateRolesFromUserList(), new PasteCopiedElementInAllLanguageMacro(), new ImportDataBase(), new DeleteChildren(), new DeleteChildrenContent(), new DuplicateChildren(), new DeleteBadTemplate(), new DownDateMacro(), new UpDateMacro(), new DeleteChildrenAndContent(), new DeleteSameComponent(), new deleteComponentInBadArea(), new UploadGallery(), new CreateBusinessComponent() });
+			new CurrentLgExpandMacro(), new EncryptPasswordComponent(), new EncryptVisitorsPasswordMacro(), new CreateRolesFromUserList(), new PasteCopiedElementInAllLanguageMacro(), new ImportDataBase(), new DeleteChildren(), new SortChildren(), new DeleteChildrenContent(), new DuplicateChildren(), new DeleteBadTemplate(), new DownDateMacro(), new UpDateMacro(), new DeleteChildrenAndContent(), new DeleteSameComponent(), new deleteComponentInBadArea(), new UploadGallery(), new CreateBusinessComponent() });
 
 	private static MacroFactory instance = null;
 
