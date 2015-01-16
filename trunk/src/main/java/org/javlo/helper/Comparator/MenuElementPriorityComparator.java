ccf55d1d1aead83f3bc82435bef37a1f17dbe890
pvandermaesen@noctis.be <pvandermaesen@noctis.be@7a838a29-3d20-a625-5319-021463a4e934>
2015-01-16 10:15:06 +0000

    no sort in sort by priority

---
+++ trunk/src/main/java/org/javlo/helper/Comparator/MenuElementPriorityComparator.java
@@ -0,0 +1,39 @@
+/*
+ * Created on Nov 11, 2004
+ */
+package org.javlo.helper.Comparator;
+
+import java.util.Comparator;
+
+import org.javlo.navigation.MenuElement;
+
+
+/**
+ * @author pvanderm
+ *
+ * this class is used for sort a array of array 
+ */
+public class MenuElementPriorityComparator implements Comparator<MenuElement> {
+	
+	private int multiply = 1;	
+	
+	public MenuElementPriorityComparator ( boolean ascending ) {		
+		if (ascending) {
+			multiply = -1;
+		}		
+	}
+	
+	public int compare(MenuElement elem1, MenuElement elem2) {
+		try {
+			int outComp = (int)Math.round((elem1.getPriority()-elem2.getPriority())*100)*multiply;
+			if (outComp != 0) {// no equality
+				return outComp;
+			} else {
+				return 1;
+			}
+		} catch (Exception e) { 
+			e.printStackTrace();
+		}
+		return 0;
+	}
+}
