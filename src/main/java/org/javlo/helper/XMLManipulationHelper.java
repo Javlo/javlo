5a7c836ede89d617636d75874bff7fd3320069f5
pvandermaesen <pvandermaesen@users.noreply.github.com>
2015-04-24 15:01:40 +0200

    new google analytics code

--- src/main/java/org/javlo/helper/XMLManipulationHelper.java
+++ src/main/java/org/javlo/helper/XMLManipulationHelper.java
@@ -535,9 +535,12 @@ public class XMLManipulationHelper {
 																																	// agregator
 
 					String previewCode = "<c:if test=\"${not contentContext.pageAssociation}\">" + getPreviewCode(globalContext.getServletContext()) + "</c:if>";
-					//remplacement.addReplacement(tags[i].getOpenEnd() + 1, tags[i].getOpenEnd() + 1, previewCode + getEscapeMenu(contentZone) + getResetTemplate() + getAfterBodyCode());
+					// remplacement.addReplacement(tags[i].getOpenEnd() + 1,
+					// tags[i].getOpenEnd() + 1, previewCode +
+					// getEscapeMenu(contentZone) + getResetTemplate() +
+					// getAfterBodyCode());
 					remplacement.addReplacement(tags[i].getOpenEnd() + 1, tags[i].getOpenEnd() + 1, getEscapeMenu(contentZone) + getResetTemplate() + getAfterBodyCode());
-					remplacement.addReplacement(tags[i].getCloseStart()-1, tags[i].getCloseStart()-1, previewCode);
+					remplacement.addReplacement(tags[i].getCloseStart() - 1, tags[i].getCloseStart() - 1, previewCode);
 				}
 
 				/* link - StyleSheet */
@@ -890,16 +893,27 @@ public class XMLManipulationHelper {
 		 * out.println("} catch(err) {}</script>");
 		 */
 
-		out.print("<%if (globalContext.getGoogleAnalyticsUACCT().length() > 4 && ctx.isTrackingContext()) {%><script type=\"text/javascript\">");
-		out.println("var _gaq = _gaq || [];");
-		out.println("_gaq.push(['_setAccount', '<%=globalContext.getGoogleAnalyticsUACCT()%>']);");
-		out.println("_gaq.push(['_trackPageview']);");
-		out.println("   (function() {");
-		out.println("      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;");
-		out.println("      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';");
-		out.println("      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);");
-		out.println("   })();");
-		out.print("</script><%}%>");
+//		out.print("<%if (globalContext.getGoogleAnalyticsUACCT().length() > 4 && ctx.isTrackingContext()) {%><script type=\"text/javascript\">");
+//		out.println("var _gaq = _gaq || [];");
+//		out.println("_gaq.push(['_setAccount', '<%=globalContext.getGoogleAnalyticsUACCT()%>']);");
+//		out.println("_gaq.push(['_trackPageview']);");
+//		out.println("   (function() {");
+//		out.println("      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;");
+//		out.println("      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';");
+//		out.println("      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);");
+//		out.println("   })();");
+//		out.print("</script><%}%>");
+
+		out.print("<script>");
+		out.println("  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){");
+		out.println("(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),");
+		out.println("m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)");
+		out.println("})(window,document,'script','//www.google-analytics.com/analytics.js','ga');");
+
+		out.println("ga('create', '<%=globalContext.getGoogleAnalyticsUACCT()%>', 'auto');");
+		out.println("ga('send', 'pageview');");
+
+		out.println("</script>");
 
 		out.close();
 		return writer.toString();
