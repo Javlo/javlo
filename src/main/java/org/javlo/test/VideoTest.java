5d00adc75199d64fc2f13430ad6667ebaeca4bfd
pvandermaesen <pvandermaesen@users.noreply.github.com>
2016-01-05 11:44:54 +0100

    change name of class

---
+++ src/main/java/org/javlo/test/VideoTest.java
@@ -0,0 +1,37 @@
+package org.javlo.test;
+
+import java.io.IOException;
+
+public class VideoTest {
+
+	public VideoTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) throws IOException {
+		/*String filename = "c:/trans/test3.mp4";
+
+		System.out.println("read : " + filename + "  exist:" + new File(filename).exists());
+		
+		IMediaReader reader = ToolFactory.makeReader(filename);
+		
+		System.out.println("reader open ? "+reader.isOpen()); */
+
+		/*IContainer container = IContainer.make();
+
+		// open up the container
+
+		if (container.open(filename, IContainer.Type.READ, null) < 0)
+			throw new IllegalArgumentException("could not open file: " + filename);
+
+		// query how many streams the call to open found
+
+		int numStreams = container.getNumStreams();
+
+		System.out.println("***** VideoTest.main : numStreams = " + numStreams); // TODO:
+																					// remove
+																					// debug
+																					// trace*/
+	}
+
+}
