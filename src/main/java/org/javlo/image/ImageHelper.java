176cbe0654a6d09a5fc8a0788cacb64219d0c9ed
pvandermaesen <pvandermaesen@noctis.be>
2017-04-18 08:58:15 +0200

    geocoder reactivation

--- src/main/java/org/javlo/image/ImageHelper.java
+++ src/main/java/org/javlo/image/ImageHelper.java
@@ -12,7 +12,6 @@ import java.awt.image.WritableRaster;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
