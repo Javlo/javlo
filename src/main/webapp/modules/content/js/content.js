53499dc99793e081b3583a75ca285c450ea4af81
pvandermaesen <pvandermaesen@noctis.be>
2018-01-04 16:11:35 +0100

    image size by default more size

--- src/main/webapp/modules/content/js/content.js
+++ src/main/webapp/modules/content/js/content.js
@@ -30,7 +30,7 @@ function loadWysiwyg(cssQuery, complexity, chooseFileURL, format, fontsize, wysi
 	}
 	
 	if (fontsize == null) {
-		fontsize = "10px 11px 12px 13px 14px 16px 18px 20px 22px 24px 26px 28px 30px 32px 34px 36px 38px 40px 42px";
+		fontsize = "10px 11px 12px 13px 14px 15px 16px 17px 18px 19px 20px 22px 24px 26px 28px 30px 32px 34px 36px 38px 40px 42px";
 	}
 	
 	if (complexity == "middle") {
