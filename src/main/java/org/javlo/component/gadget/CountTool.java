b9c0877fbcfcd1279838255e0f838c71899e3fe7
Patrick Vandermaesen <pvandermaesen@immanence.be>
2018-09-26 13:26:59 +0200

    cout animation

--- src/main/java/org/javlo/component/gadget/CountTool.java
+++ src/main/java/org/javlo/component/gadget/CountTool.java
@@ -6,14 +6,15 @@ package org.javlo.component.gadget;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.text.ParseException;
-import java.util.Calendar;
+import java.util.Arrays;
 import java.util.Date;
-import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.logging.Logger;
 
-import org.javlo.component.core.AbstractVisualComponent;
+import org.javlo.component.properties.AbstractPropertiesComponent;
 import org.javlo.context.ContentContext;
 import org.javlo.helper.StringHelper;
+import org.javlo.helper.URLHelper;
 import org.javlo.i18n.I18nAccess;
 import org.javlo.message.GenericMessage;
 import org.javlo.service.RequestService;
@@ -21,7 +22,7 @@ import org.javlo.service.RequestService;
 /**
  * @author pvandermaesen
  */
-public class CountTool extends AbstractVisualComponent {
+public class CountTool extends AbstractPropertiesComponent {
 
 	/**
 	 * create a static logger.
@@ -30,18 +31,19 @@ public class CountTool extends AbstractVisualComponent {
 
 	public static final String TYPE = "counttool";
 
-	@Override
-	public String getPrefixViewXHTMLCode(ContentContext ctx) {
-		return "<div " + getSpecialPreviewCssClass(ctx, getComponentCssClass(ctx)) + getSpecialPreviewCssId(ctx) + " >";
-	}
+	private static final String FIELD_TARGET_DATE = "target-date";
+	private static final String FIELD_LINK = "link";
+	private static final String FIELD_LABEL = "label";
 
-	@Override
-	public String getSuffixViewXHTMLCode(ContentContext ctx) {
-		return "</div>";
-	}
+	private static final List<String> FIELDS = Arrays.asList(new String[] { FIELD_TARGET_DATE, FIELD_LINK, FIELD_LABEL });
 
 	protected String getDateName() {
 		return "date_" + getId();
+	}
+
+	@Override
+	public List<String> getFields(ContentContext ctx) throws Exception {
+		return FIELDS;
 	}
 
 	protected String getLinkName() {
@@ -57,93 +59,33 @@ public class CountTool extends AbstractVisualComponent {
 	}
 
 	@Override
-	protected String getEditXHTMLCode(ContentContext ctx) throws Exception {
-		StringWriter writer = new StringWriter();
-		PrintWriter out = new PrintWriter(writer);
-
-		I18nAccess i18nAccess = I18nAccess.getInstance(ctx.getRequest());
-
-		out.println(getSpecialInputTag());
-		out.println("<div class=\"line\">");
-		out.print("<label for=\"" + getDateName() + "\">");
-		out.print(i18nAccess.getText("content.counttool.date"));
-		out.println(" : </label>");
-		out.print("<input id=\"" + getDateName() + "\" name=\"" + getDateName() + "\" value=\"");
-		out.print(StringHelper.neverNull(StringHelper.renderTime(getDate())));
-		out.println("\" />");
-		out.println("</div>");
-		out.println("<div class=\"line\">");
-		out.print("<label for=\"" + getLinkName() + "\">");
-		out.print(i18nAccess.getText("content.counttool.link"));
-		out.println(" : </label>");
-		out.print("<input id=\"" + getLinkName() + "\" name=\"" + getLinkName() + "\" value=\"");
-		out.print(getLink());
-		out.println("\" />");
-		out.println("</div>");
-
-		out.close();
-
-		// validation
-		if (getDate() == null) {
-			setMessage(new GenericMessage(i18nAccess.getText("content.counttool.unvalid-date"), GenericMessage.ERROR));
-		} else {
-			setMessage(null);
-		}
-
-		return writer.toString();
-
+	public void prepareView(ContentContext ctx) throws Exception {
+		super.prepareView(ctx);
+		Date date = getDate();
+		ctx.getRequest().setAttribute("targetDate", date.getTime());
+		Date currentDate = new Date();
+		ctx.getRequest().setAttribute("second", Math.round((date.getTime() - currentDate.getTime()) / 1000));
+		ctx.getRequest().setAttribute("link", URLHelper.convertLink(ctx,getFieldValue(FIELD_LINK)));
+		ctx.getRequest().setAttribute("label", getFieldValue(FIELD_LABEL));
 	}
 
 	public Date getDate() {
-		String[] allValue = StringHelper.stringToArray(getValue());
-
+		String rawDate = getFieldValue(FIELD_TARGET_DATE);
 		Date date = null;
-		if (allValue.length > 0 && allValue[0].length() > 0) {
+		try {
+			date = StringHelper.parseTime(rawDate);
+		} catch (ParseException e) {
 			try {
-				date = StringHelper.parseTime(allValue[0]);
-			} catch (ParseException e) {
-				try {
-					date = StringHelper.parseDate(allValue[0]);
-				} catch (ParseException e1) {
-					// return null = error
-				}
+				date = StringHelper.parseDate(rawDate);
+			} catch (ParseException e1) {
 			}
 		}
-
 		return date;
 	}
 
 	@Override
 	public String getViewXHTMLCode(ContentContext ctx) throws Exception {
-		if (getDate() == null) {
-			return "";
-		}
-		StringWriter writer = new StringWriter();
-		PrintWriter out = new PrintWriter(writer);
-		Calendar cal = GregorianCalendar.getInstance();
-		I18nAccess i18nAccess = I18nAccess.getInstance(ctx.getRequest());
-		i18nAccess.changeViewLanguage(ctx);
-		String js = "";
-		if (getLink().length() > 0) {
-			out.println("<a href=\"" + getLink() + "\">");
-		}
-		out.println("<div" + js + " class=\"count-tool count-tool-" + ctx.getRequestContentLanguage() + "\">");
-		out.println("	<input type=\"text\" class=\"timer-days\" readonly=\"readonly\" />");
-		out.println("	<input type=\"text\" class=\"timer-time\" readonly=\"readonly\" />");
-		out.println("	<script language=\"javascript\">");
-		cal.setTime(new Date());
-		out.println("		var currentDate = new Date(" + cal.get(Calendar.YEAR) + "," + cal.get(Calendar.MONTH) + "," + cal.get(Calendar.DAY_OF_MONTH) + "," + cal.get(Calendar.HOUR) + "," + cal.get(Calendar.MINUTE) + "," + cal.get(Calendar.SECOND) + ");");
-		cal.setTime(getDate());
-		out.println("		var endDate = new Date(" + cal.get(Calendar.YEAR) + "," + cal.get(Calendar.MONTH) + "," + cal.get(Calendar.DAY_OF_MONTH) + "," + cal.get(Calendar.HOUR_OF_DAY) + "," + cal.get(Calendar.MINUTE) + "," + cal.get(Calendar.SECOND) + ");");
-		out.println("		displayDate('" + i18nAccess.getContentViewText("global.days") + "');displayDate.periodical(1000);");
-		out.println("	</script>");
-		out.println("</div>");
-		if (getLink().length() > 0) {
-			out.println("</a>");
-		}
-
-		out.close();
-		return writer.toString();
+		return "no renderer defined";
 	}
 
 	@Override
@@ -153,7 +95,7 @@ public class CountTool extends AbstractVisualComponent {
 
 	@Override
 	public int getComplexityLevel(ContentContext ctx) {
-		return COMPLEXITY_ADMIN;
+		return COMPLEXITY_STANDARD;
 	}
 
 	@Override
@@ -162,30 +104,8 @@ public class CountTool extends AbstractVisualComponent {
 	}
 
 	@Override
-	public String performEdit(ContentContext ctx) throws Exception {
-		RequestService requestService = RequestService.getInstance(ctx.getRequest());
-		String date = requestService.getParameter(getDateName(), "");
-		String link = requestService.getParameter(getLinkName(), null);
-		if (link == null) {
-			return null;
-		}
-		if (!link.equals(getLink())) {
-			setModify();
-		} else if (date.length() > 0) {
-			try {
-				Date dateParsed = StringHelper.parseTime(date);
-				if (!dateParsed.equals(getDate())) {
-					setModify();
-				}
-			} catch (ParseException e) {
-				logger.warning(e.getMessage());
-			}
-		}
-		if (isModify()) {
-			String[] values = new String[] { date, link };
-			setValue(StringHelper.arrayToString(values));
-		}
-		return null;
+	public String getFontAwesome() {
+		return "clock-o";
 	}
-	
+
 }
