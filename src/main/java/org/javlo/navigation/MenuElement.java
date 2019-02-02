5e16369f63ceb79ffe79c89894dcdf205b090576
Patrick Vandermaesen <pvandermaesen@immanence.be>
2019-02-02 21:03:53 +0100

    resolve page title

--- src/main/java/org/javlo/navigation/MenuElement.java
+++ src/main/java/org/javlo/navigation/MenuElement.java
@@ -3574,6 +3574,7 @@ public class MenuElement implements Serializable, IPrintInfo, IRestItem, ITaxono
 		newCtx.setFree(true);
 
 		PageDescription desc = getPageDescriptionCached(ctx, newCtx.getRequestContentLanguage());
+		
 
 		if (desc.pageTitle != null) {
 			// return desc.pageTitle;
