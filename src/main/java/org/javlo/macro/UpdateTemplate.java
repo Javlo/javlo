764a02c29d9c94f18d811c40b697e51383f2c679
VANDERMAESEN Patrick <patrick.vandermaesen@ext.europarl.europa.eu>
2021-03-19 09:46:43 +0100

    update template

---
+++ src/main/java/org/javlo/macro/UpdateTemplate.java
@@ -0,0 +1,54 @@
+package org.javlo.macro;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import org.javlo.context.ContentContext;
+import org.javlo.helper.ResourceHelper;
+import org.javlo.helper.StringHelper;
+import org.javlo.template.Template;
+
+public class UpdateTemplate extends AbstractMacro {
+	
+	private static Logger logger = Logger.getLogger(UpdateTemplate.class.getName());
+
+	@Override
+	public String getName() {
+		return "update-template";
+	}
+
+	@Override
+	public String perform(ContentContext ctx, Map<String, Object> params) throws Exception {
+		Template template = ctx.getCurrentTemplate();
+		logger.info("template : "+template.getName());
+		template.importTemplateInWebapp(ctx.getGlobalContext().getStaticConfig(), ctx, false);
+		template.reload();		
+		Collection<File> allFiles = ResourceHelper.getAllFilesList(new File(template.getWorkTemplateRealPath(ctx.getGlobalContext())));
+		long olderModifDate = 0;
+		for (File file : allFiles) {
+			if (StringHelper.getFileExtension(file.getName()).equalsIgnoreCase("scss")) {
+				if (file.lastModified() > olderModifDate) {
+					olderModifDate = file.lastModified();
+				}
+			}
+		}
+		for (File file : allFiles) {
+			if (StringHelper.getFileExtension(file.getName()).equalsIgnoreCase("scss")) {
+				File cssFile = new File(StringHelper.getFileNameWithoutExtension(file.getAbsolutePath())+".css");
+				if (cssFile.exists() && cssFile.lastModified() < olderModifDate) {
+					logger.info("delete : "+cssFile);
+					cssFile.delete();
+				}
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public boolean isPreview() {
+		return true;
+	}
+
+};
