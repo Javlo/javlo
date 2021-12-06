15f9e7320a96d42a7f8ca7b9897740517f276903
VANDERMAESEN Patrick <patrick.vandermaesen@ext.europarl.europa.eu>
2021-12-06 16:33:49 +0100

    generic exception

---
+++ src/main/java/org/javlo/exception/NotImplementedException.java
@@ -0,0 +1,13 @@
+package org.javlo.exception;
+
+public class NotImplementedException extends Exception {
+	
+	public NotImplementedException() {
+		super();
+	}
+	
+	public NotImplementedException(String msg) {
+		super(msg);
+	}
+
+}
