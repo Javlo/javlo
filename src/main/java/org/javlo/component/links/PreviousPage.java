84d4b244309e0861d1ab433bc91edea602daea3e
VANDERMAESEN Patrick <patrick.vandermaesen@ext.europarl.europa.eu>
2022-01-18 15:51:21 +0100

    previous link secondary by default

--- src/main/java/org/javlo/component/links/PreviousPage.java
+++ src/main/java/org/javlo/component/links/PreviousPage.java
@@ -49,7 +49,7 @@ public class PreviousPage extends ComplexPropertiesLink {
 		if (!isHidden(ctx)) {
 			ByteArrayOutputStream outStream = new ByteArrayOutputStream();
 			PrintStream out = new PrintStream(outStream);
-			out.println("<a href=\""+previousPage.getUrl()+"\" class=\"btn btn-primary\">"+ctx.getRequest().getAttribute("label")+"</a>");
+			out.println("<a href=\""+previousPage.getUrl()+"\" class=\"btn btn-secondary\">"+ctx.getRequest().getAttribute("label")+"</a>");
 			out.close();
 			return new String(outStream.toByteArray());
 		} else {
