1c1592408ce90bb0fa977cf791c8ef71221a9ec6
VANDERMAESEN Patrick <patrick.vandermaesen@ext.europarl.europa.eu>
2022-03-08 11:20:13 +0100

    merci file with string temp

--- src/main/java/org/javlo/utils/DocxUtils.java
+++ src/main/java/org/javlo/utils/DocxUtils.java
@@ -8,16 +8,14 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -32,6 +30,7 @@ import org.javlo.component.text.WysiwygParagraph;
 import org.javlo.component.title.Heading;
 import org.javlo.context.ContentContext;
 import org.javlo.context.GlobalContext;
+import org.javlo.helper.ResourceHelper;
 import org.javlo.helper.URLHelper;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -87,7 +86,8 @@ public class DocxUtils {
 		return false;
 	}
 
-	public static List<ComponentBean> extractContent(GlobalContext globalContext, InputStream in, String resourceFolder) throws XDocConverterException, IOException {
+	public static List<ComponentBean> extractContent(GlobalContext globalContext, InputStream in, String resourceFolder)
+			throws XDocConverterException, IOException {
 		Options options = Options.getFrom(DocumentKind.DOCX).to(ConverterTypeTo.XHTML);
 		IConverter converter = ConverterRegistry.getRegistry().getConverter(options);
 
@@ -130,11 +130,13 @@ public class DocxUtils {
 						text = StringEscapeUtils.unescapeXml(StringEscapeUtils.escapeHtml4(text));
 						bean.setValue("text=" + text + "\ndepth=" + titleLevel);
 					}
-				} else if (item.tagName().equals("img") && item.attr("src") != null && item.attr("src").trim().length() > 0) {
+				} else if (item.tagName().equals("img") && item.attr("src") != null
+						&& item.attr("src").trim().length() > 0) {
 					ByteArrayOutputStream outStream = new ByteArrayOutputStream();
 					PrintStream outPrint = new PrintStream(outStream);
 					String folder = item.attr("src");
-					outPrint.println("dir=" + URLHelper.mergePath(resourceFolder, new File(folder).getParentFile().getPath()));
+					outPrint.println(
+							"dir=" + URLHelper.mergePath(resourceFolder, new File(folder).getParentFile().getPath()));
 					outPrint.println("file-name=" + new File(folder).getName());
 					outPrint.println(GlobalImage.IMAGE_FILTER + "=full");
 					if (item.attr("alt") != null) {
@@ -158,16 +160,49 @@ public class DocxUtils {
 	}
 
 	public static void main(String[] args) throws IOException {
-		File docxFile = new File("c:/trans/modele de convention.docx");
-		Map<String, String> tokens = new HashMap<>();
-		tokens.put("${company.name}", "Ma Myrtille à moi !");
-		tokens.put("${company.web}", "https://lescontesdemyrtille.be/");
-		replaceTokens(docxFile.getAbsolutePath(), tokens);
+		//File docxFile = new File("c:/trans/modele de convention.docx");
+		File docxFile = new File("c:/trans/REISOVEREENKOMST INDICAMP WIP.docx");
+		
+//		Map<String, String> tokens = new HashMap<>();
+//		tokens.put("${company.name}", "Ma Myrtille à moi !");
+//		tokens.put("${company.web}", "https://lescontesdemyrtille.be/");
+//		replaceTokens(docxFile.getAbsolutePath(), tokens);
+		
+		String xml = getDocxXmlContent(docxFile.getAbsolutePath());
+		xml = xml.replace("{{firstname}}", "Patrick aime l'été");
+		
+		System.out.println(xml);
+		
+		writeDocxXmlContent(docxFile.getAbsolutePath(), xml);
+		
+	}
+
+	public static String getDocxXmlContent(String docxFile) throws IOException {
+		Path zipFilePath = Paths.get(docxFile);
+		try (FileSystem fs = FileSystems.newFileSystem(zipFilePath, null)) {
+			Path source = fs.getPath("/word/document.xml");
+			try (InputStream in = Files.newInputStream(source);) {
+				return ResourceHelper.writeStreamToString(in, "UTF-8");
+			}
+		}
+	}
+
+	public static void writeDocxXmlContent(String docxFile, String xml) throws IOException {
+		Path zipFilePath = Paths.get(docxFile);
+		try (FileSystem fs = FileSystems.newFileSystem(zipFilePath, null)) {
+			Path source = fs.getPath("/word/document.xml");
+			if (Files.exists(source)) {
+				Files.delete(source);
+			}
+			try (OutputStream out = Files.newOutputStream(source);) {
+				ResourceHelper.writeStringToStream(xml, out, "UTF-8");
+			}
+		}
 	}
 
 	public static void replaceTokens(String docxFile, Map<String, String> tokens) throws IOException {
 		Path zipFilePath = Paths.get(docxFile);
-		try (FileSystem fs = FileSystems.newFileSystem(zipFilePath, null)) {
+		try (FileSystem fs = FileSystems.	newFileSystem(zipFilePath, null)) {
 
 //			Files.walkFileTree(fs.getPath("/"), new SimpleFileVisitor<Path>() {
 //				@Override
@@ -187,7 +222,8 @@ public class DocxUtils {
 			// throw new IOException("temp file exists, generate another name");
 			// }
 			Files.move(source, temp);
-			try (BufferedReader br = new BufferedReader(new InputStreamReader(Files.newInputStream(temp))); BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(Files.newOutputStream(source)))) {
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(Files.newInputStream(temp)));
+					BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(Files.newOutputStream(source)))) {
 				String line;
 				while ((line = br.readLine()) != null) {
 					for (Map.Entry<String, String> token : tokens.entrySet()) {
