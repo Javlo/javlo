5201214fabac5dc213c742b5c065fb8e2d7869b8
VANDERMAESEN Patrick <patrick.vandermaesen@ext.europarl.europa.eu>
2022-03-19 13:14:34 +0100

    multi users

--- src/main/java/org/javlo/component/users/DisplayUserInfo.java
+++ src/main/java/org/javlo/component/users/DisplayUserInfo.java
@@ -2,6 +2,8 @@ package org.javlo.component.users;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.javlo.actions.IAction;
 import org.javlo.component.core.AbstractVisualComponent;
@@ -38,55 +40,67 @@ public class DisplayUserInfo extends AbstractVisualComponent implements IAction 
 	public void prepareView(ContentContext ctx) throws Exception {
 		super.prepareView(ctx);
 
-		User user = UserFactory.createUserFactory(ctx.getRequest()).getUser(getValue());
-		if (user == null) {
+		List<User> users = new LinkedList<>();
+
+		List<String> logins = StringHelper.stringToCollection(getValue(), ",");
+		for (String login : logins) {
+			User user = UserFactory.createUserFactory(ctx.getRequest()).getUser(login);
+			if (user != null) {
+				users.add(user);
+			}
+		}
+
+		if (users.size() == 0) {
 			MessageRepository messageRepository = MessageRepository.getInstance(ctx);
-			messageRepository.setGlobalMessage(new GenericMessage("user not found : " + getValue(), GenericMessage.ERROR));
+			messageRepository.setGlobalMessage(new GenericMessage("user(s) not found : " + getValue(), GenericMessage.ERROR));
 		} else {
-			ctx.getRequest().setAttribute("userToDisplay", user);
+			ctx.getRequest().setAttribute("usersToDisplay", users);
 		}
 	}
-	
+
 	protected boolean getColumnableDefaultValue() {
 		return true;
 	}
 
 	@Override
 	public String getViewXHTMLCode(ContentContext ctx) throws Exception {
-		User user = (User) ctx.getRequest().getAttribute("userToDisplay");
-		if (user == null) {
+		List<User> users = (List<User>) ctx.getRequest().getAttribute("usersToDisplay");
+		if (users == null) {
 			prepareView(ctx);
-			user = (User) ctx.getRequest().getAttribute("userToDisplay");
+			users = (List<User>) ctx.getRequest().getAttribute("usersToDisplay");
 		}
-		if (user != null) {
-			ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-			PrintStream out = new PrintStream(outStream);
-			out.println("<div class=\"row\"><div class=\"col\"><ul class=\"list-group\">");
-			if (!StringHelper.isEmpty(user.getUserInfo().getFirstName()) || !StringHelper.isEmpty(user.getUserInfo().getLastName())) {
-				out.println("<li class=\"list-group-item\">"+user.getUserInfo().getFirstName()+' '+user.getUserInfo().getLastName()+"</li>");
-			}
-			if (!StringHelper.isEmpty(user.getUserInfo().getEmail())) {
-				out.println("<li class=\"list-group-item\"><a href=\"mailto:"+user.getUserInfo().getEmail()+"\">"+user.getUserInfo().getEmail()+"</a></li>");
-			}
-			if (user.getUserInfo() instanceof UserInfo) {
-				UserInfo userInfo = (UserInfo)user.getUserInfo();
-				if (!StringHelper.isEmpty(userInfo.getMobile())) {
-					out.println("<li class=\"list-group-item\"><a href=\"tel:"+userInfo.getMobile()+"\">"+userInfo.getMobile()+"</a></li>");
+		if (users.size() == 0) {
+			return "user(s) not found : " + getValue();
+		}
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		PrintStream out = new PrintStream(outStream);
+		for (User user : users) {
+			if (user != null) {
+				out.println("<div class=\"row user-item mb-3\"><div class=\"col\"><ul class=\"list-group\">");
+				if (!StringHelper.isEmpty(user.getUserInfo().getFirstName()) || !StringHelper.isEmpty(user.getUserInfo().getLastName())) {
+					out.println("<li class=\"list-group-item\">" + user.getUserInfo().getFirstName() + ' ' + user.getUserInfo().getLastName() + "</li>");
 				}
-			}
-			out.println("</ul></div>");
-			
+				if (!StringHelper.isEmpty(user.getUserInfo().getEmail())) {
+					out.println("<li class=\"list-group-item\"><a href=\"mailto:" + user.getUserInfo().getEmail() + "\">" + user.getUserInfo().getEmail() + "</a></li>");
+				}
+				if (user.getUserInfo() instanceof UserInfo) {
+					UserInfo userInfo = (UserInfo) user.getUserInfo();
+					if (!StringHelper.isEmpty(userInfo.getMobile())) {
+						out.println("<li class=\"list-group-item\"><a href=\"tel:" + userInfo.getMobile() + "\">" + userInfo.getMobile() + "</a></li>");
+					}
+				}
+				out.println("</ul></div>");
+
 				out.println("<div class=\"col-3\">");
 				InfoBean info = InfoBean.getCurrentInfoBean(ctx);
-				out.println("<img src=\""+info.getAvatarURL().get(user.getUserInfo())+"\" class=\"img-thumbnail\" alt=\""+user.getUserInfo().getFirstName()+"\">");
+				out.println("<img src=\"" + info.getAvatarURL().get(user.getUserInfo()) + "\" class=\"img-thumbnail\" alt=\"" + user.getUserInfo().getFirstName() + "\">");
 				out.println("</div>");
-			
-			out.println("</div>");
-			out.close();
-			return new String(outStream.toByteArray());
-		} else {
-			return "user not found : "+getValue();
+
+				out.println("</div>");
+			}
 		}
+		out.close();
+		return new String(outStream.toByteArray());
 	}
 
 	@Override
