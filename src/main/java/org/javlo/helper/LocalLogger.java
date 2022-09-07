b7513eee2768f84a40334b7cd79f78bb9d8b655f
VANDERMAESEN Patrick <patrick.vandermaesen@ext.europarl.europa.eu>
2022-09-07 16:48:35 +0200

    display time in second

--- src/main/java/org/javlo/helper/LocalLogger.java
+++ src/main/java/org/javlo/helper/LocalLogger.java
@@ -106,7 +106,7 @@ public class LocalLogger {
 		if (oldTime == null) {
 			System.out.println("key not found : " + key);
 		} else {
-			log(DEBUG, "[FSTEP-COUNT] - "+'(' + key + ") " + message + " [" + (System.currentTimeMillis() - oldTime.longValue()) + " ms ]");
+			log(DEBUG, "[FSTEP-COUNT] - "+'(' + key + ") " + message + " [" + StringHelper.renderTimeInSecond(System.currentTimeMillis() - oldTime.longValue()) + " sec ]");
 		}
 	}
 
@@ -119,7 +119,7 @@ public class LocalLogger {
 			oldTime = (long) -1;
 		}
 
-		log(DEBUG, "[STEP-COUNT] - " + message + " [" + StringHelper.renderTimeInSecond(System.currentTimeMillis() - oldTime.longValue()) + " ms ]");
+		log(DEBUG, "[STEP-COUNT] - " + message + " [" + StringHelper.renderTimeInSecond(System.currentTimeMillis() - oldTime.longValue()) + " sec ]");
 	}
 
 	public static void endCount(String key, String message) {
@@ -131,7 +131,7 @@ public class LocalLogger {
 			System.out.println("key not found : " + key);
 		} else {
 			times.remove(key);
-			log(DEBUG, "[END-COUNT] - " + message + " [" + (System.currentTimeMillis() - oldTime.longValue()) + " ms ]");
+			log(DEBUG, "[END-COUNT] - " + message + " [" + StringHelper.renderTimeInSecond(System.currentTimeMillis() - oldTime.longValue()) + " sec ]");
 		}
 	}
 
