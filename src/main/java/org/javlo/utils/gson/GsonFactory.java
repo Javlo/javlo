c4fcdc927bc2961ed06631617cdfe4392bc1364a
VANDERMAESEN Patrick <patrick.vandermaesen@ext.europarl.europa.eu>
2023-02-11 14:28:41 +0100

    buidl default json

--- src/main/java/org/javlo/utils/gson/GsonFactory.java
+++ src/main/java/org/javlo/utils/gson/GsonFactory.java
@@ -1,6 +1,7 @@
 package org.javlo.utils.gson;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -8,7 +9,7 @@ import com.google.gson.GsonBuilder;
 public class GsonFactory {
 	
 	public static Gson getGson() {
-		return new GsonBuilder().setDateFormat("yyyy-MM-dd").registerTypeAdapter(LocalDate.class, new LocalDateAdapterGson()).registerTypeAdapter(LocalDate.class, new LocalDateTimeAdapterGson()).create();
+		return new GsonBuilder().setDateFormat("yyyy-MM-dd").registerTypeAdapter(LocalDate.class, new LocalDateAdapterGson()).registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapterGson()).create();
 	}
 
 }
