dcf7daa5d0548a163d073fd2b0de596bd658d160
VANDERMAESEN Patrick <patrick.vandermaesen@ext.europarl.europa.eu>
2023-03-11 09:10:42 +0100

    interface color template

--- src/main/java/org/javlo/config/StaticConfig.java
+++ src/main/java/org/javlo/config/StaticConfig.java
@@ -2022,7 +2022,7 @@ public class StaticConfig extends Observable {
 	}
 
 	public List<String> getPreviewLayout() {
-		return StringHelper.stringToCollection(properties.getString("preview.layout", "light,dark"), ",");
+		return StringHelper.stringToCollection(properties.getString("preview.layout", "light,dark,pink,sea"), ",");
 	}
 
 	public int getBackupInterval() {
