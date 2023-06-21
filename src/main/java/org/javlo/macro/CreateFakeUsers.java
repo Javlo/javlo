f58249969225f95f8be9e0b4e2c76061586e56e0
VANDERMAESEN Patrick <patrick.vandermaesen@ext.europarl.europa.eu>
2023-06-21 08:29:23 +0200

    add phone and mobile

--- src/main/java/org/javlo/macro/CreateFakeUsers.java
+++ src/main/java/org/javlo/macro/CreateFakeUsers.java
@@ -3,10 +3,20 @@ package org.javlo.macro;
 import org.javlo.context.ContentContext;
 import org.javlo.helper.SecurityHelper;
 import org.javlo.helper.StringHelper;
+import org.javlo.helper.URLHelper;
+import org.javlo.image.ImageEngine;
 import org.javlo.user.IUserFactory;
 import org.javlo.user.UserFactory;
 import org.javlo.user.UserInfo;
+import org.javlo.ztatic.FileCache;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.*;
 
 public class CreateFakeUsers extends AbstractMacro {
@@ -17,7 +27,7 @@ public class CreateFakeUsers extends AbstractMacro {
 
 	@Override
 	public String getName() {
-		return "create-10-fake-users";
+		return "create-3-fake-users";
 	}
 
 	@Override
@@ -32,7 +42,7 @@ public class CreateFakeUsers extends AbstractMacro {
 			lastName.add(name.split(" ")[1].trim());
 		}
 		int userNumber = 1;
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 3; i++) {
 			UserInfo userInfo = new UserInfo();
 			String userName = "login"+StringHelper.renderNumber(userNumber, 4);
 			while (userFactory.getUser(userName) != null) {
@@ -45,12 +55,52 @@ public class CreateFakeUsers extends AbstractMacro {
 			userInfo.setLastName(lastName.get((int) Math.round(Math.random() * (lastName.size()-1))));
 			userInfo.setOrganization(COMPAGNIES.get((int) Math.round(Math.random() * (COMPAGNIES.size()-1))));
 			userInfo.setEmail(userInfo.getFirstName().toLowerCase() + '.' + userInfo.getLastName().toLowerCase() + "@javlo.org");
+			userInfo.setPhone(generateRandomPhoneNumber());
+			userInfo.setMobile(generateRandomMobileNumber());
 			userInfo.setRoles(new HashSet<String>(Arrays.asList(new String[] { "test" })));
 			userFactory.addUserInfo(userInfo);
+			createAvatar(ctx, userInfo);
 		}
 		userFactory.store();
 
 		return null;
+	}
+
+	public static String generateRandomPhoneNumber() {
+		Random random = new Random();
+		int firstThreeDigits = random.nextInt(900) + 100; // Génère un nombre aléatoire entre 100 et 999
+		int secondTwoDigits = random.nextInt(90) + 10; // Génère un nombre aléatoire entre 10 et 99
+		int thirdTwoDigits = random.nextInt(90) + 10; // Génère un nombre aléatoire entre 10 et 99
+
+		return "+32 2 " + firstThreeDigits + " " + secondTwoDigits + " " + thirdTwoDigits;
+	}
+
+	public static String generateRandomMobileNumber() {
+		Random random = new Random();
+		int firstThreeDigits = random.nextInt(90) + 10; // Génère un nombre aléatoire entre 10 et 99
+		int secondTwoDigits = random.nextInt(90) + 10; // Génère un nombre aléatoire entre 10 et 99
+		int thirdTwoDigits = random.nextInt(90) + 10; // Génère un nombre aléatoire entre 10 et 99
+
+		return "+32 475 " + firstThreeDigits + " " + secondTwoDigits + " " + thirdTwoDigits;
+	}
+
+	public void createAvatar(ContentContext ctx, UserInfo userInfo) throws IOException {
+		String avatarFileName = userInfo.getLogin() + ".png";
+		File avatarFile = new File(URLHelper.mergePath(ctx.getGlobalContext().getDataFolder(), ctx.getGlobalContext().getStaticConfig().getAvatarFolder(), avatarFileName));
+		URL url = new URL("https://thispersondoesnotexist.com/");
+		URLConnection conn = url.openConnection();
+		try (InputStream in = conn.getInputStream()) {
+			BufferedImage img = ImageIO.read(in);
+			img = ImageEngine.resizeWidth(img, 255, true);
+			avatarFile.getParentFile().mkdirs();
+			ImageIO.write(img, "png", avatarFile);
+		}
+		FileCache.getInstance(ctx.getRequest().getSession().getServletContext()).deleteAllFile(ctx.getGlobalContext().getContextKey(), avatarFileName);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -58,4 +108,8 @@ public class CreateFakeUsers extends AbstractMacro {
 		return false;
 	}
 
+	@Override
+	public boolean isAdmin() {
+		return true;
+	}
 }
