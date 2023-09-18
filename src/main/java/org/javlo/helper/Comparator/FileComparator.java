4807c2b94c5c52dbb8897c04da81d35f4571a610
VANDERMAESEN Patrick <patrick.vandermaesen@ext.europarl.europa.eu>
2023-09-18 13:49:16 +0200

    compare correction

--- src/main/java/org/javlo/helper/Comparator/FileComparator.java
+++ src/main/java/org/javlo/helper/Comparator/FileComparator.java
@@ -30,35 +30,28 @@ public class FileComparator implements Comparator<File> {
 	 */
 	@Override
 	public int compare(File file1, File file2) {
+		int diff = 0;
+
 		switch (sortType) {
-		case NAME:
-			int diff = file1.getName().compareTo(file2.getName());
-			
-			if (ascending) {
-				return diff;
-			} else {
-				return -diff;
-			}
-		case LASTMODIFIED:
-			diff = (int) (file1.lastModified() - file2.lastModified());
-			
-			if (ascending) {
-				return diff;
-			} else {
-				return -diff;
-			}
-		case SIZE:
-			if (ascending) {
-				diff = (int) (file1.length() - file2.length());
-				
-				if (ascending) {
-					return diff;
-				} else {
-					return -diff;
-				}
-			}
-		default:
-			return file1.compareTo(file2);
+			case NAME:
+				diff = file1.getName().compareTo(file2.getName());
+				break;
+			case LASTMODIFIED:
+				long timeDiff = file1.lastModified() - file2.lastModified();
+				diff = timeDiff > 0 ? 1 : (timeDiff < 0 ? -1 : 0); // évite le dépassement d'int
+				break;
+			case SIZE:
+				long sizeDiff = file1.length() - file2.length();
+				diff = sizeDiff > 0 ? 1 : (sizeDiff < 0 ? -1 : 0); // évite le dépassement d'int
+				break;
+			default:
+				return file1.compareTo(file2);
 		}
+
+		if (!ascending) {
+			diff = -diff;
+		}
+
+		return diff;
 	}
 }
