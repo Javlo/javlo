6219b26736847caff414a4d22bfb4a9c250f132b
VANDERMAESEN Patrick <patrick.vandermaesen@ext.europarl.europa.eu>
2024-02-05 22:03:25 +0100

    qrcode generator

--- src/main/java/org/javlo/servlet/QRCodeServlet.java
+++ src/main/java/org/javlo/servlet/QRCodeServlet.java
@@ -1,28 +1,27 @@
  package org.javlo.servlet;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.util.logging.Logger;
+ import jakarta.servlet.ServletException;
+ import jakarta.servlet.http.HttpServlet;
+ import jakarta.servlet.http.HttpServletRequest;
+ import jakarta.servlet.http.HttpServletResponse;
+ import net.glxn.qrgen.QRCode;
+ import net.glxn.qrgen.image.ImageType;
+ import org.apache.commons.lang3.StringUtils;
+ import org.javlo.component.core.IContentVisualComponent;
+ import org.javlo.component.core.ILink;
+ import org.javlo.context.ContentContext;
+ import org.javlo.context.GlobalContext;
+ import org.javlo.helper.StringHelper;
+ import org.javlo.helper.URLHelper;
+ import org.javlo.navigation.MenuElement;
+ import org.javlo.service.ContentService;
 
-import javax.imageio.ImageIO;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import org.apache.commons.lang3.StringUtils;
-import org.javlo.component.core.IContentVisualComponent;
-import org.javlo.component.core.ILink;
-import org.javlo.context.ContentContext;
-import org.javlo.context.GlobalContext;
-import org.javlo.helper.URLHelper;
-import org.javlo.navigation.MenuElement;
-import org.javlo.service.ContentService;
-
-import net.glxn.qrgen.QRCode;
-import net.glxn.qrgen.image.ImageType;
+ import javax.imageio.ImageIO;
+ import java.awt.image.BufferedImage;
+ import java.io.ByteArrayInputStream;
+ import java.io.ByteArrayOutputStream;
+ import java.io.File;
+ import java.util.logging.Logger;
 
 public class QRCodeServlet extends HttpServlet {
 
@@ -58,72 +57,86 @@ public class QRCodeServlet extends HttpServlet {
 		String category = splittedPath[0];
 		String file = splittedPath[1];
 		String[] splittedFile = StringUtils.split(file, ".");
+
+
 		
 		try {
 			ContentContext ctx = ContentContext.getContentContext(request, response);
-			
-			if (category.endsWith("_preview")) {
-				ctx.setRenderMode(ContentContext.PREVIEW_MODE);
-				category = category.substring(0, category.lastIndexOf("_preview"));
-			}
 
-			String data = null;
-
-			if (category.equals("link")) {
-				ContentService content = ContentService.getInstance(GlobalContext.getInstance(request));
-				IContentVisualComponent comp = content.getComponent(ctx, splittedFile[0]);
-				if (comp == null) {
-					logger.warning("component not found : " + splittedFile[0]);
-					response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-					return;
-				} else if (!(comp instanceof ILink)) {
-					logger.warning("component not link : " + splittedFile[0]);
-					response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-					return;
-				}
-				ctx = ctx.getContextForAbsoluteURL();
-				data = ((ILink) comp).getURL(ctx);
-			} else if (category.equals("data")) {
-				data = (String)ctx.getGlobalContext().getTimeAttribute(splittedFile[0]);
-			} else if (category.equals("page")) {
-				String url = request.getPathInfo().replaceFirst("/page/", "");
-				String lg = url.substring(0,2);
-				url = url.substring(2);
-				ByteArrayOutputStream out = new ByteArrayOutputStream();
-				
-				ContentContext pageCtx = new ContentContext(ctx);
-				pageCtx.setAbsoluteURL(true);
-				pageCtx.setAllLanguage(lg);
-				
-				pageCtx.setPath(url);
-				MenuElement page = ctx.getCurrentPage();
-				
-				if (page.isRoot()) {
-					logger.warning("page root ? : "+url);
-				}
-				
-				if (page == null) {
-					logger.warning("page not found : "+url);
-					response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-					return;
-				} else {
-					String shortUrl = URLHelper.createStaticURL(ctx.getContextForAbsoluteURL(), pageCtx.getCurrentPage().getShortLanguageURL(pageCtx));
-					QRCode.from(shortUrl).to(ImageType.PNG).withSize(1024+74,1024+74).writeTo(out); //74 = estimation of margin
+			if (request.getParameter("link") != null) {
+				if (StringHelper.isTrue(ctx.getGlobalContext().getStaticConfig().getProperty("qrcode.free", null), false)) {
+					ByteArrayOutputStream out = new ByteArrayOutputStream();
+					String data = request.getParameter("link");
+					QRCode.from(data).to(ImageType.PNG).withSize(1024, 1024).writeTo(out); //74 = estimation of margin
 					BufferedImage image = ImageIO.read(new ByteArrayInputStream(out.toByteArray()));
-					//image = removeBorder(image);
+					image = removeBorder(image);
 					ImageIO.write(image, "png", response.getOutputStream());
 				}
-			}
+			} else {
 
-			if (data != null) {				
-				int qrSize = ctx.getCurrentTemplate().getQRCodeSize(); 
-				
-				ByteArrayOutputStream out = new ByteArrayOutputStream();
-				QRCode.from(data).to(ImageType.PNG).withSize(qrSize+74,qrSize+74).writeTo(out); //74 = estimation of margin
-				
-				BufferedImage image = ImageIO.read(new ByteArrayInputStream(out.toByteArray()));
-				image = removeBorder(image);
-				ImageIO.write(image, splittedFile[1], response.getOutputStream());
+				if (category.endsWith("_preview")) {
+					ctx.setRenderMode(ContentContext.PREVIEW_MODE);
+					category = category.substring(0, category.lastIndexOf("_preview"));
+				}
+
+				String data = null;
+
+				if (category.equals("link")) {
+					ContentService content = ContentService.getInstance(GlobalContext.getInstance(request));
+					IContentVisualComponent comp = content.getComponent(ctx, splittedFile[0]);
+					if (comp == null) {
+						logger.warning("component not found : " + splittedFile[0]);
+						response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+						return;
+					} else if (!(comp instanceof ILink)) {
+						logger.warning("component not link : " + splittedFile[0]);
+						response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+						return;
+					}
+					ctx = ctx.getContextForAbsoluteURL();
+					data = ((ILink) comp).getURL(ctx);
+				} else if (category.equals("data")) {
+					data = (String) ctx.getGlobalContext().getTimeAttribute(splittedFile[0]);
+				} else if (category.equals("page")) {
+					String url = request.getPathInfo().replaceFirst("/page/", "");
+					String lg = url.substring(0, 2);
+					url = url.substring(2);
+					ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+					ContentContext pageCtx = new ContentContext(ctx);
+					pageCtx.setAbsoluteURL(true);
+					pageCtx.setAllLanguage(lg);
+
+					pageCtx.setPath(url);
+					MenuElement page = ctx.getCurrentPage();
+
+					if (page.isRoot()) {
+						logger.warning("page root ? : " + url);
+					}
+
+					if (page == null) {
+						logger.warning("page not found : " + url);
+						response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+						return;
+					} else {
+						String shortUrl = URLHelper.createStaticURL(ctx.getContextForAbsoluteURL(), pageCtx.getCurrentPage().getShortLanguageURL(pageCtx));
+						QRCode.from(shortUrl).to(ImageType.PNG).withSize(1024 + 74, 1024 + 74).writeTo(out); //74 = estimation of margin
+						BufferedImage image = ImageIO.read(new ByteArrayInputStream(out.toByteArray()));
+						//image = removeBorder(image);
+						ImageIO.write(image, "png", response.getOutputStream());
+					}
+				}
+
+				if (data != null) {
+					int qrSize = ctx.getCurrentTemplate().getQRCodeSize();
+
+					ByteArrayOutputStream out = new ByteArrayOutputStream();
+					QRCode.from(data).to(ImageType.PNG).withSize(qrSize + 74, qrSize + 74).writeTo(out); //74 = estimation of margin
+
+					BufferedImage image = ImageIO.read(new ByteArrayInputStream(out.toByteArray()));
+					image = removeBorder(image);
+					ImageIO.write(image, splittedFile[1], response.getOutputStream());
+				}
 			}
 
 		} catch (Exception e) {
