84cd9db72df04b5fa8a0c74c424439a919cf58ee
pvandermaesen <pvandermaesen@noctis.be>
2024-10-10 10:57:01 +0200

    add trace

--- src/main/java/org/javlo/macro/EncryptVisitorsPasswordMacro.java
+++ src/main/java/org/javlo/macro/EncryptVisitorsPasswordMacro.java
@@ -1,14 +1,14 @@
 package org.javlo.macro;
 
-import java.util.Collection;
-import java.util.Map;
-
 import org.javlo.context.ContentContext;
 import org.javlo.context.GlobalContext;
 import org.javlo.helper.StringHelper;
 import org.javlo.user.IUserFactory;
 import org.javlo.user.IUserInfo;
 import org.javlo.user.UserFactory;
+
+import java.util.Collection;
+import java.util.Map;
 
 /**
  * merge component meta data defined in the template and meta data define in content (only meta data, don't touch to data)
@@ -17,6 +17,8 @@ import org.javlo.user.UserFactory;
  * 
  */
 public class EncryptVisitorsPasswordMacro extends AbstractMacro {
+
+	protected static java.util.logging.Logger logger = java.util.logging.Logger.getLogger(EncryptVisitorsPasswordMacro.class.getName());
 
 	@Override
 	public String getName() {
@@ -28,14 +30,17 @@ public class EncryptVisitorsPasswordMacro extends AbstractMacro {
 		GlobalContext globalContext = GlobalContext.getInstance(ctx.getRequest());
 		IUserFactory userFactory = UserFactory.createUserFactory(globalContext, ctx.getRequest().getSession());
 		Collection<IUserInfo> allUserInfo = userFactory.getUserInfoList();
+		int c = 0;
 		for (IUserInfo iUserInfo : allUserInfo) {
 			if (iUserInfo.getPassword() != null && iUserInfo.getPassword().trim().length() > 0) {
+				logger.info("encrypt password : "+iUserInfo.getLogin());
 				iUserInfo.setPassword(StringHelper.encryptPassword(iUserInfo.getPassword()));
+				c++;
 			}
 		}
 		userFactory.store();
 
-		return null;
+		return c+" password encrypted.";
 	}
 
 	@Override
