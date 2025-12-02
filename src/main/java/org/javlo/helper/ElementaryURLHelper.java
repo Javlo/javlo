b40d591a873781cbfa023da50fb973125b0188b3
pvandermaesen <pvandermaesen@noctis.be>
2025-12-02 14:25:42 +0100

    add_section to image name

--- src/main/java/org/javlo/helper/ElementaryURLHelper.java
+++ src/main/java/org/javlo/helper/ElementaryURLHelper.java
@@ -504,7 +504,7 @@ public abstract class ElementaryURLHelper {
 				if (staticInfo != null && !StringHelper.isEmpty(staticInfo.getTitle(ctx))) {
 					fileName = staticInfo.getTitle(ctx);
 				}
-				fileName = file.getParentFile().getName()+"_"+fileName;
+				fileName = StringHelper.toMaxSize(file.getParentFile().getName(), 9,"") + '_' + fileName;
 				url = URLHelper.mergePath(IMG_SERVLET_PATH, ctx.getGlobalContext().setTransformShortURL(url.replace(TRANSFORM + '/', ""), filter, fileName));
 			}
 		}
