cba95e9ae25378e72807ab8262ab5b6a4e0a8c6f
pvandermaesen <pvandermaesen@noctis.be>
2025-12-15 15:44:01 +0100

    2.3.4.1

--- src/main/java/org/javlo/servlet/IVersion.java
+++ src/main/java/org/javlo/servlet/IVersion.java
@@ -2,5 +2,5 @@ package org.javlo.servlet;
 
 public interface IVersion {
 	public static final String NAME = "Javlo2";
-	public static final String VERSION = "2.3.4";
+	public static final String VERSION = "2.3.4.1";
 }
