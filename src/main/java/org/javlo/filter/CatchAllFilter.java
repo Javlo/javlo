154a0cd6553bd705d1190052bd9bfffeb83a841e
pvandermaesen <pvandermaesen@noctis.be>
2026-02-10 09:45:45 +0100

    block ip display uri

--- src/main/java/org/javlo/filter/CatchAllFilter.java
+++ src/main/java/org/javlo/filter/CatchAllFilter.java
@@ -173,7 +173,7 @@ public class CatchAllFilter implements Filter {
 		/** security : block ip attack **/
 		if (AccessSecurity.getInstance(httpRequest).isIpBlock(httpRequest)) {
 			httpResponse.reset();
-			logger.severe("BLOCK IP # = "+BLOCK_IP+" ["+httpRequest.getRequestURI()+"]");
+			logger.severe("BLOCK IP # = "+BLOCK_IP+" ["+NetHelper.getIp(httpRequest)+" > "+httpRequest.getRequestURI()+"]");
 			httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 			BLOCK_IP++;
 			return;
